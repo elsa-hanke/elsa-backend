645b0a43e8a95218781cf68676e429cc599f12d8
Sauli Anto <sauli.anto@gofore.com>
2020-10-08 14:24:53 +0300

    Konfiguroitu uudelleenohjausta

--- src/main/kotlin/fi/oulu/elsa/web/rest/Redirect2FrontendResource.kt
+++ src/main/kotlin/fi/oulu/elsa/web/rest/Redirect2FrontendResource.kt
@@ -1,17 +1,24 @@
 package fi.oulu.elsa.web.rest
 
 import io.github.jhipster.config.JHipsterConstants
-import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.view.RedirectView
+import javax.servlet.http.HttpServletRequest
 
 @RestController
-@Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
-class Redirect2FrontendResource() {
+class Redirect2FrontendResource(private val env: Environment) {
 
     @GetMapping("/")
-    fun redirect2FrontendView(): RedirectView? {
-        return RedirectView("http://localhost:9060/")
+    fun redirect2FrontendView(request: HttpServletRequest): RedirectView? {
+        // TODO: Toteuta parempi tapa ohjata takaisin Web-sovellukseen
+        val activeProfiles = env.activeProfiles
+        return if (activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)) {
+            RedirectView("http://localhost:9060/")
+        } else {
+            val url = request.requestURL.toString()
+            RedirectView(url.replace("api.", ""))
+        }
     }
 }
