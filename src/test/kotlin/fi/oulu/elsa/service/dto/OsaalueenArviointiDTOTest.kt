5dc7154abc8be0478a92295ef0525d2a47be8651
Sauli Anto <sauli.anto@gmail.com>
2020-10-12 10:52:24 +0300

    Lisätty puuttuvat luokat

---
+++ src/test/kotlin/fi/oulu/elsa/service/dto/OsaalueenArviointiDTOTest.kt
@@ -0,0 +1,23 @@
+package fi.oulu.elsa.service.dto
+
+import fi.oulu.elsa.web.rest.equalsVerifier
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class OsaalueenArviointiDTOTest {
+
+    @Test
+    fun dtoEqualsVerifier() {
+        equalsVerifier(OsaalueenArviointiDTO::class)
+        val osaalueenArviointiDTO1 = OsaalueenArviointiDTO()
+        osaalueenArviointiDTO1.id = 1L
+        val osaalueenArviointiDTO2 = OsaalueenArviointiDTO()
+        assertThat(osaalueenArviointiDTO1).isNotEqualTo(osaalueenArviointiDTO2)
+        osaalueenArviointiDTO2.id = osaalueenArviointiDTO1.id
+        assertThat(osaalueenArviointiDTO1).isEqualTo(osaalueenArviointiDTO2)
+        osaalueenArviointiDTO2.id = 2L
+        assertThat(osaalueenArviointiDTO1).isNotEqualTo(osaalueenArviointiDTO2)
+        osaalueenArviointiDTO1.id = null
+        assertThat(osaalueenArviointiDTO1).isNotEqualTo(osaalueenArviointiDTO2)
+    }
+}
