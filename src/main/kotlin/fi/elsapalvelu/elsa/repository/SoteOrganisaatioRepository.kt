c00e1679f93ac35eebde69bffec6ccad8cfed12b
Sauli Anto <sauli.anto@gofore.com>
2020-10-22 22:00:46 +0300

    Lisätty rajapinta SOTE-organisaatioille

---
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/SoteOrganisaatioRepository.kt
@@ -0,0 +1,10 @@
+package fi.elsapalvelu.elsa.repository
+
+import fi.elsapalvelu.elsa.domain.SoteOrganisaatio
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
+
+@Suppress("unused")
+@Repository
+interface SoteOrganisaatioRepository : JpaRepository<SoteOrganisaatio, String>, JpaSpecificationExecutor<SoteOrganisaatio>
