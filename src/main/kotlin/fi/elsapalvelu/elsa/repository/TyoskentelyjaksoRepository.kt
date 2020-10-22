2166fe75fe66d6f5e4a0e3ffea047ea750c3647c
Sauli Anto <sauli.anto@gofore.com>
2020-10-22 22:02:17 +0300

    Korjattu suoritusarvioinnin testit

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/TyoskentelyjaksoRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/TyoskentelyjaksoRepository.kt
@@ -4,9 +4,6 @@ import fi.elsapalvelu.elsa.domain.Tyoskentelyjakso
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-/**
- * Spring Data  repository for the [Tyoskentelyjakso] entity.
- */
 @Suppress("unused")
 @Repository
 interface TyoskentelyjaksoRepository : JpaRepository<Tyoskentelyjakso, Long>
