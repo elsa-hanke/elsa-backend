35e1496359c689e52723dc2f1f61baec5862a0f6
Sauli Anto <sauli.anto@gofore.com>
2020-10-26 22:25:07 +0200

    Toteutettu Suoritusarviointi rajapintoja

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/SuoritusarviointiRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/SuoritusarviointiRepository.kt
@@ -5,15 +5,20 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
-@Suppress("unused")
 @Repository
 interface SuoritusarviointiRepository : JpaRepository<Suoritusarviointi, Long> {
 
     fun findAllByTyoskentelyjaksoErikoistuvaLaakariId(id: Long, pageable: Pageable): Page<Suoritusarviointi>
 
     fun findAllByTyoskentelyjaksoErikoistuvaLaakariKayttajaUserId(
-        id: String,
+        userId: String,
         pageable: Pageable
     ): Page<Suoritusarviointi>
+
+    fun findOneByIdAndTyoskentelyjaksoErikoistuvaLaakariKayttajaUserId(
+        id: Long,
+        userId: String
+    ): Optional<Suoritusarviointi>
 }
