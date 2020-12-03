ee7886fd7744b130561ad4e316ef3e6497584900
Sauli Anto <sauli.anto@gmail.com>
2020-12-03 09:40:47 +0200

    UOELSA-108: Lisätty puuttuvat valinnat

--- src/main/kotlin/fi/elsapalvelu/elsa/service/dto/SuoritemerkintaFormDTO.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/dto/SuoritemerkintaFormDTO.kt
@@ -6,6 +6,10 @@ class SuoritemerkintaFormDTO(
 
     var tyoskentelyjaksot: MutableSet<TyoskentelyjaksoDTO> = mutableSetOf(),
 
+    var kunnat: MutableSet<KuntaDTO> = mutableSetOf(),
+
+    var erikoisalat: MutableSet<ErikoisalaDTO> = mutableSetOf(),
+
     var oppimistavoitteenKategoriat: MutableSet<OppimistavoitteenKategoriaDTO> = mutableSetOf()
 
 ) : Serializable
