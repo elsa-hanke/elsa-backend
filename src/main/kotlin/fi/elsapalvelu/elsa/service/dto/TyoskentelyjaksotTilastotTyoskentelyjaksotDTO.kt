26d4b0231efca938d73cf6774b689ea6619f593e
Sauli Anto <sauli.anto@gofore.com>
2020-12-15 13:03:16 +0200

    UOELSA-176: Toteutettu työskentelyajan laskemista

---
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/dto/TyoskentelyjaksotTilastotTyoskentelyjaksotDTO.kt
@@ -0,0 +1,11 @@
+package fi.elsapalvelu.elsa.service.dto
+
+import java.io.Serializable
+
+class TyoskentelyjaksotTilastotTyoskentelyjaksotDTO(
+
+    var id: Long,
+
+    var suoritettu: Double,
+
+) : Serializable
