a9d13a66f110624e7b05e3a08c9eb2f3f44b829e
Arttu Hänninen <arttu.hanninen@gofore.com>
2021-04-15 09:22:11 +0300

    UOELSA-396: SMTP config and email templates

---
+++ src/main/kotlin/fi/elsapalvelu/elsa/config/ThymeleafConfiguration.kt
@@ -0,0 +1,22 @@
+package fi.elsapalvelu.elsa.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
+
+import org.thymeleaf.templateresolver.ITemplateResolver
+
+
+@Configuration
+class ThymeleafConfiguration {
+
+    @Bean
+    fun thymeleafTemplateResolver(): ITemplateResolver? {
+        val templateResolver = ClassLoaderTemplateResolver()
+        templateResolver.prefix = "templates/mail/"
+        templateResolver.suffix = ".html"
+        templateResolver.setTemplateMode("HTML")
+        templateResolver.characterEncoding = "UTF-8"
+        return templateResolver
+    }
+}
