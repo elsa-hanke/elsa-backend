69d2cb9a2040d165cad2f164447311dd2e1f89e5
Arttu Hänninen <arttu.hanninen@gofore.com>
2021-04-20 10:31:46 +0300

    UOELSA-423: Koejakson päänäkymä kouluttajalle

--- src/main/kotlin/fi/elsapalvelu/elsa/service/KoejaksonAloituskeskusteluService.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/KoejaksonAloituskeskusteluService.kt
@@ -1,5 +1,6 @@
 package fi.elsapalvelu.elsa.service
 
+import fi.elsapalvelu.elsa.service.dto.KayttajaDTO
 import fi.elsapalvelu.elsa.service.dto.KoejaksonAloituskeskusteluDTO
 import java.util.*
 
@@ -30,6 +31,10 @@ interface KoejaksonAloituskeskusteluService {
         id: Long,
         userId: String
     ): Optional<KoejaksonAloituskeskusteluDTO>
+
+    fun findAllByKouluttajaUserId(
+        userId: String
+    ): Map<KayttajaDTO, KoejaksonAloituskeskusteluDTO>
 
     fun delete(id: Long)
 }
