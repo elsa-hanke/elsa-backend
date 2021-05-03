b79443204b721c2d9f5d4a9416b73fd0b971d733
Tero Viljakainen <tero.viljakainen@gofore.com>
2021-05-03 16:52:53 +0300

    UOELSA-46: Asiakirjat
    - Add new table asiakirjat
    - Implement create, read and delete operations
    - Store asiakirja data as a blob in database
    - Add mapping for fetching by tyoskentelyjakso id
    - Add projection to ignore asiakirja data when fetching results for listing

---
+++ src/main/kotlin/fi/elsapalvelu/elsa/domain/Asiakirja.kt
@@ -0,0 +1,63 @@
+package fi.elsapalvelu.elsa.domain
+
+import com.sun.istack.NotNull
+import org.hibernate.annotations.Cache
+import org.hibernate.annotations.CacheConcurrencyStrategy
+import java.io.Serializable
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "asiakirja")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+data class Asiakirja(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    var id: Long? = null,
+
+    @ManyToOne(optional = false)
+    @JoinColumn(unique = true)
+    var erikoistuvaLaakari: ErikoistuvaLaakari? = null,
+
+    @ManyToOne(optional = true)
+    @JoinColumn(unique = true)
+    var tyoskentelyjakso: Tyoskentelyjakso? = null,
+
+    @NotNull
+    @Column(name = "nimi", nullable = false)
+    var nimi: String? = null,
+
+    @NotNull
+    @Column(name = "tyyppi", nullable = false)
+    var tyyppi: String? = null,
+
+    @NotNull
+    @Column(name = "lisattypvm", nullable = false)
+    var lisattypvm : LocalDateTime? = null,
+
+    @NotNull
+    @Column(name = "data", nullable = false)
+    var data: ByteArray? = null
+
+) : Serializable {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Asiakirja) return false
+
+        return id != null && other.id != null && id == other.id
+    }
+
+    override fun hashCode() = 31
+
+    override fun toString() = "Asiakirja{" +
+        "id=$id" +
+        ", nimi='$nimi'" +
+        "}"
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+}
