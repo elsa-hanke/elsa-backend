10d3c4296aeeb1a121134433a8b0321bbcf0f3f7
Tero Viljakainen <tero.viljakainen@gofore.com>
2021-06-17 15:30:22 +0300

    UOELSA-510: Refaktoroitu koejakson vaiheiden haku - Tehty uusi service KoejaksonVaiheetService, joka koostaa DTO:n - Haetaan koejakson vaiheet viimeisestä kouluttajalle näkyvästä vaiheesta alaspäin ja lisätään kunkin vaiheen kohdalla myös aiemmat vaiheet objektiin - Mikäli vaihe on jo lisätty aiemman vaiheen koostamisen yhteydessä ei vaihetta haetaan enää uudestaan kannasta - Päivitä KouluttajaKoejaksoResourceIT ottamaan huomioon, että kaikki vaiheet sekä niiden hyväksytyt vaiheet palautuvat endpointilta

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/KoejaksonValiarviointiRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/KoejaksonValiarviointiRepository.kt
@@ -2,7 +2,6 @@ package fi.elsapalvelu.elsa.repository
 
 import fi.elsapalvelu.elsa.domain.KoejaksonValiarviointi
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -20,20 +19,6 @@ interface KoejaksonValiarviointiRepository : JpaRepository<KoejaksonValiarvioint
     ): Optional<KoejaksonValiarviointi>
 
     fun findByErikoistuvaLaakariKayttajaUserId(userId: String): Optional<KoejaksonValiarviointi>
-
-    @Query(
-        "select v from KoejaksonValiarviointi v " +
-            "where v.erikoistuvaLaakari = (" +
-            "select k.erikoistuvaLaakari from KoejaksonKehittamistoimenpiteet k where k.id = :id)"
-    )
-    fun findByKehittamistoimenpiteetId(id: Long): Optional<KoejaksonValiarviointi>
-
-    @Query(
-        "select v from KoejaksonValiarviointi v " +
-            "where v.erikoistuvaLaakari = (" +
-            "select l.erikoistuvaLaakari from KoejaksonLoppukeskustelu l where l.id = :id)"
-    )
-    fun findByLoppukeskusteluId(id: Long): Optional<KoejaksonValiarviointi>
 
     fun findAllByLahikouluttajaUserIdOrLahiesimiesUserId(
         kouluttajaId: String,
