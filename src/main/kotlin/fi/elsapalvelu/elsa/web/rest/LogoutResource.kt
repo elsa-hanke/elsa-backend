8dfc0b39427f82e6878e71e81e39b659ced1f731
Arttu Hänninen <arttu.hanninen@gofore.com>
2021-06-23 11:08:45 +0300

    UOELSA-475: Single logout

--- src/main/kotlin/fi/elsapalvelu/elsa/web/rest/LogoutResource.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/web/rest/LogoutResource.kt
@@ -1,18 +1,25 @@
 package fi.elsapalvelu.elsa.web.rest
 
+import fi.elsapalvelu.elsa.security.logout.OpenSamlLogoutRequestResolver
+import fi.elsapalvelu.elsa.security.logout.Saml2LogoutRequest
+import fi.elsapalvelu.elsa.security.logout.Saml2LogoutRequestResolver
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal
-import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticationToken
+import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication
+import org.springframework.util.Assert
+import org.springframework.util.StringUtils
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.util.UriComponentsBuilder
+import org.springframework.web.util.UriUtils
+import java.nio.charset.StandardCharsets
+import java.security.Principal
 import javax.servlet.http.HttpServletRequest
 
 /**
  * REST controller for managing global OIDC logout.
  */
 @RestController
-class LogoutResource {
+class LogoutResource(private val logoutRequestResolver: OpenSamlLogoutRequestResolver) {
 
     /**
      * `POST  /api/logout` : logout the current user.
@@ -21,18 +28,43 @@ class LogoutResource {
      * @param idToken the ID token.
      * @return the [ResponseEntity] with status `200 (OK)` and a body with a global logout URL and ID token.
      */
-    @PostMapping("/api/logout25")
+    @PostMapping("/api/logout")
     fun logout(
         request: HttpServletRequest,
-        @AuthenticationPrincipal(expression = "idToken") idToken: Saml2AuthenticationToken?
+        principal: Principal?,
     ): ResponseEntity<*> {
-        val logoutUrl = "asfd"
+        val builder: Saml2LogoutRequestResolver.Saml2LogoutRequestBuilder<*> =
+            this.logoutRequestResolver.resolveLogoutRequest(
+                request,
+                principal as Saml2Authentication
+            )
+        val logoutRequest = builder.logoutRequest()
+        val location: String = logoutRequest!!.location
+        val uriBuilder = UriComponentsBuilder.fromUriString(location)
+        addParameter("SAMLRequest", logoutRequest, uriBuilder)
+        addParameter("RelayState", logoutRequest, uriBuilder)
+        addParameter("SigAlg", logoutRequest, uriBuilder)
+        addParameter("Signature", logoutRequest, uriBuilder)
+        val logoutUrl = uriBuilder.build(true).toUriString()
 
         val logoutDetails = mutableMapOf(
-            "logoutUrl" to logoutUrl,
-            "idToken" to idToken
+            "logoutUrl" to logoutUrl
         )
         request.session.invalidate()
         return ResponseEntity.ok().body(logoutDetails)
+    }
+
+    private fun addParameter(
+        name: String,
+        logoutRequest: Saml2LogoutRequest,
+        builder: UriComponentsBuilder
+    ) {
+        Assert.hasText(name, "name cannot be empty or null")
+        if (StringUtils.hasText(logoutRequest.getParameter(name))) {
+            builder.queryParam(
+                UriUtils.encode(name, StandardCharsets.ISO_8859_1),
+                UriUtils.encode(logoutRequest.getParameter(name)!!, StandardCharsets.ISO_8859_1)
+            )
+        }
     }
 }
