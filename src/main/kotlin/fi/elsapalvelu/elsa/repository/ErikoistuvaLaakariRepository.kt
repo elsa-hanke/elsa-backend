6289b98ffd6e6a6d457411498af95766fd8eb42a
Sauli Anto <sauli.anto@gofore.com>
2021-09-02 17:11:28 +0300

    UOELSA-224: Näytetään erikoistuvalle vain ne EPA-osaamisalueet jotka ovat olleet voimassa erikoistumisen alussa

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/ErikoistuvaLaakariRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/ErikoistuvaLaakariRepository.kt
@@ -3,7 +3,6 @@ package fi.elsapalvelu.elsa.repository
 import fi.elsapalvelu.elsa.domain.ErikoistuvaLaakari
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface ErikoistuvaLaakariRepository : JpaRepository<ErikoistuvaLaakari, Long> {
