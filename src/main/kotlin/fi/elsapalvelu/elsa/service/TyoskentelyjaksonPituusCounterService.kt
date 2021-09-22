9c8f14b6c92a248678e86c9a5d376eec4a219908
Tero Viljakainen <tero.viljakainen@gofore.com>
2021-09-22 09:49:18 +0300

    UOELSA-467: Refaktoroi TyoskentelyjaksonPituusCounterService
    - Lisää calculateHyvaksiluettavatDaysLeft metodi jota yli 100% työskentelyn tarkastamiseen tehtävä validointi voi käyttää

--- src/main/kotlin/fi/elsapalvelu/elsa/service/TyoskentelyjaksonPituusCounterService.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/TyoskentelyjaksonPituusCounterService.kt
@@ -2,10 +2,18 @@ package fi.elsapalvelu.elsa.service
 
 import fi.elsapalvelu.elsa.domain.Tyoskentelyjakso
 import fi.elsapalvelu.elsa.service.dto.HyvaksiluettavatCounterData
+import java.time.LocalDate
 
 interface TyoskentelyjaksonPituusCounterService {
 
-    fun calculateInDays(tyoskentelyjakso: Tyoskentelyjakso, hyvaksiluettavatCounterData: HyvaksiluettavatCounterData): Double
+    fun calculateInDays(
+        tyoskentelyjakso: Tyoskentelyjakso,
+        hyvaksiluettavatCounterData: HyvaksiluettavatCounterData
+    ): Double
+
+    fun calculateHyvaksiluettavatDaysLeft(
+        tyoskentelyjaksot: List<Tyoskentelyjakso>, calculateUntilDate: LocalDate? = null
+    ): HyvaksiluettavatCounterData
 
     fun getHyvaksiluettavatPerYearMap(tyoskentelyjaksot: List<Tyoskentelyjakso>): MutableMap<Int, Double>
 
