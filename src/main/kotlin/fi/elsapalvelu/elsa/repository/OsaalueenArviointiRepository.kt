d76843902d44865fefbdb19b8b52a4143348e866
Tero Viljakainen <tero.viljakainen@gofore.com>
2021-10-07 10:48:23 +0300

    UOELSA-729: Poista OsaalueenArviointi
    - Poista taulu osaalueen_arviointi sekä siihen liittyvä koodi koska kyseistä entiteettiä ei käytetä missään

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/OsaalueenArviointiRepository.kt
+++
@@ -1,8 +0,0 @@
-package fi.elsapalvelu.elsa.repository
-
-import fi.elsapalvelu.elsa.domain.OsaalueenArviointi
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface OsaalueenArviointiRepository : JpaRepository<OsaalueenArviointi, Long>
