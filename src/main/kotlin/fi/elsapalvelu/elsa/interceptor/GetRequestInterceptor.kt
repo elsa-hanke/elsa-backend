c8ce86cc710760a649245c13ede9382a19c48168
Tero Viljakainen <tero.viljakainen@gofore.com>
2021-10-19 13:19:52 +0300

    UOELSA-762: Lisää interceptor GET-requesteille
    - Lokita käyttäjän id, pyydetyn resurssin URI sekä päivämäärä ja kellonaika

---
+++ src/main/kotlin/fi/elsapalvelu/elsa/interceptor/GetRequestInterceptor.kt
@@ -0,0 +1,28 @@
+package fi.elsapalvelu.elsa.interceptor
+
+import fi.elsapalvelu.elsa.security.SecurityLoggingWrapper
+import org.springframework.security.core.AuthenticatedPrincipal
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class GetRequestInterceptor : HandlerInterceptor {
+
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        if (request.method != "GET") return true
+        val context = SecurityContextHolder.getContext()
+        if (context.authentication is Saml2Authentication) {
+            val authentication = context.authentication as Saml2Authentication
+            val principal = authentication.principal as AuthenticatedPrincipal
+            SecurityLoggingWrapper.info(
+                "SECURITY: GET request for ${request.requestURI}, user id: ${principal.name}"
+            )
+        }
+        return true
+    }
+
+}
