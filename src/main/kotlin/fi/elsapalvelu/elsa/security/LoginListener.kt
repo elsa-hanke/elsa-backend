877e4d2d5889d33a5ce66f18738f5ea5a90a57ae
Tero Viljakainen <tero.viljakainen@gofore.com>
2021-10-19 10:57:04 +0300

    UOELSA-761: Lokita käyttäjän id ja IP-osoite onnistuneen sisäänkirjautumisen jälkeen

---
+++ src/main/kotlin/fi/elsapalvelu/elsa/security/LoginListener.kt
@@ -0,0 +1,20 @@
+package fi.elsapalvelu.elsa.security
+
+import org.springframework.context.ApplicationListener
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal
+import org.springframework.stereotype.Component
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
+
+@Component
+class LoginListener : ApplicationListener<InteractiveAuthenticationSuccessEvent> {
+
+    override fun onApplicationEvent(event: InteractiveAuthenticationSuccessEvent) {
+        val principal = event.authentication.principal as Saml2AuthenticatedPrincipal
+        val requestAttributes = RequestContextHolder.getRequestAttributes() as ServletRequestAttributes
+        val sourceIpAddress = requestAttributes.request.remoteAddr
+        SecurityLoggingWrapper.info("SECURITY: Authentication success event: " +
+            "User id: ${principal.name}. Source IP address: $sourceIpAddress")
+    }
+}
