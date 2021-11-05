a1a2e84e7a1acb1699091cb461d1a0d9c5831bcc
tviljaka <81751595+tviljaka@users.noreply.github.com>
2021-11-05 12:06:00 +0200

    UOELSA-791: Hae vastuuhenkilöt erikoistuvan erikoisalan ja yliopiston mukaan

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/KayttajaRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/KayttajaRepository.kt
@@ -11,10 +11,17 @@ interface KayttajaRepository : JpaRepository<Kayttaja, Long> {
 
     fun findOneByUserId(id: String): Optional<Kayttaja>
 
-    @Query("select k from Kayttaja k join k.user u left join u.authorities a where a.name = :authority")
-    fun findAllByUserAuthority(authority: String): MutableList<Kayttaja>
+    @Query("select k from Kayttaja k join k.user u left join u.authorities a where a.name in :authorities")
+    fun findAllByUserAuthorities(authorities: List<String>): MutableList<Kayttaja>
 
-    @Query("select k from Kayttaja k join k.user u left join u.authorities a where a.name = :authority " +
-        "and k.yliopisto.id = :yliopistoId")
-    fun findAllByUserAuthorityAndYliopistoId(authority: String, yliopistoId: Long?): MutableList<Kayttaja>
+    @Query(
+        "select k from Kayttaja k join k.user u left join u.authorities a left join k.yliopistot y " +
+            "left join k.erikoisalat e where a.name in :authorities " +
+            "and :yliopistoId in (y) and :erikoisalaId in (e)"
+    )
+    fun findAllByAuthoritiesAndYliopistoAndErikoisala(
+        authorities: List<String>,
+        yliopistoId: Long?,
+        erikoisalaId: Long?
+    ): MutableList<Kayttaja>
 }
