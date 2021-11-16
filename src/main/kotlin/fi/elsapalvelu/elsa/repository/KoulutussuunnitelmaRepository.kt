fb7316f808b3ca6862fcc6eb89d66185151d4fa4
tviljaka <81751595+tviljaka@users.noreply.github.com>
2021-11-16 14:56:28 +0200

    UOELSA-850: Koulutussuunnitelmaan liitetyn asiakirjan poisto
    - Tarkista asiakirjaa poistettaessa, onko koulutussuunnitelmasta viittaus siihen. Jos viittaus löytyy, poista se.

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/KoulutussuunnitelmaRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/KoulutussuunnitelmaRepository.kt
@@ -2,10 +2,15 @@ package fi.elsapalvelu.elsa.repository
 
 import fi.elsapalvelu.elsa.domain.Koulutussuunnitelma
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface KoulutussuunnitelmaRepository : JpaRepository<Koulutussuunnitelma, Long> {
 
     fun findOneByErikoistuvaLaakariId(erikoistuvaLaakariId: Long): Koulutussuunnitelma?
+
+    @Query("select k from Koulutussuunnitelma k where k.koulutussuunnitelmaAsiakirja.id = :id or " +
+        "k.motivaatiokirjeAsiakirja.id = :id")
+    fun findOneByKoulutussuunnitelmaAsiakirjaIdOrMotivaatiokirjeAsiakirjaId(id: Long): Koulutussuunnitelma?
 }
