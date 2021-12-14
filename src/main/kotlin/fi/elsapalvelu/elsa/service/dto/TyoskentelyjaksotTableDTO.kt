914c149cd2489f4de7cb76b28811cd50f59ccbbe
tviljaka <81751595+tviljaka@users.noreply.github.com>
2021-12-14 16:51:04 +0200

    UOELSA-641: Poista poissaolon syyt TyoskentelyjaksotTableDTO:lta

--- src/main/kotlin/fi/elsapalvelu/elsa/service/dto/TyoskentelyjaksotTableDTO.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/dto/TyoskentelyjaksotTableDTO.kt
@@ -4,8 +4,6 @@ import java.io.Serializable
 
 class TyoskentelyjaksotTableDTO(
 
-    var poissaolonSyyt: MutableSet<PoissaolonSyyDTO> = mutableSetOf(),
-
     var tyoskentelyjaksot: MutableSet<TyoskentelyjaksoDTO> = mutableSetOf(),
 
     var keskeytykset: MutableSet<KeskeytysaikaDTO> = mutableSetOf(),
