e204d2eb0379f8d5b4e43f73fefcd036aea5ce44
Arttu Hänninen <arttu.hanninen@gofore.com>
2021-12-21 10:47:46 +0200

    UOELSA-786: Opinto-oikeuden täytyy olla aktiivinen

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/OpintooikeusRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/OpintooikeusRepository.kt
@@ -2,11 +2,25 @@ package fi.elsapalvelu.elsa.repository
 
 import fi.elsapalvelu.elsa.domain.Opintooikeus
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 
 
 @Repository
 interface OpintooikeusRepository : JpaRepository<Opintooikeus, Long> {
 
-    fun existsByErikoistuvaLaakariKayttajaUserId(userId: String): Boolean
+    @Query(
+        """
+        select case when count(o) > 0 then true else false end from Opintooikeus o
+        join o.erikoistuvaLaakari e
+        join e.kayttaja k
+        join k.user u
+        where :paiva between o.opintooikeudenMyontamispaiva and o.opintooikeudenPaattymispaiva and u.id = :userId
+        """
+    )
+    fun existsByErikoistuvaLaakariKayttajaUserId(
+        userId: String,
+        paiva: LocalDate
+    ): Boolean
 }
