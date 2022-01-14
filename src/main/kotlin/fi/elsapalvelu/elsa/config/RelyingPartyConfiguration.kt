e6916e140a2767260d70bd3335f22453e11ada97
Arttu Hänninen <arttu.hanninen@gofore.com>
2022-01-14 17:29:48 +0200

    UOELSA-789: Jaa hakan metadata osiin, jotta spring security osaa rakentaa relyingpartyt oikein

--- src/main/kotlin/fi/elsapalvelu/elsa/config/RelyingPartyConfiguration.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/config/RelyingPartyConfiguration.kt
@@ -10,8 +10,17 @@ import org.springframework.core.io.ResourceLoader
 import org.springframework.security.converter.RsaKeyConverters
 import org.springframework.security.saml2.core.Saml2X509Credential
 import org.springframework.security.saml2.provider.service.registration.*
+import java.io.*
+import java.net.URL
 import java.security.cert.X509Certificate
 import java.security.interfaces.RSAPrivateKey
+import javax.xml.stream.XMLEventWriter
+import javax.xml.stream.XMLInputFactory
+import javax.xml.stream.XMLOutputFactory
+import javax.xml.stream.XMLStreamConstants.END_ELEMENT
+import javax.xml.stream.XMLStreamConstants.START_ELEMENT
+import javax.xml.stream.events.EndElement
+import javax.xml.stream.events.StartElement
 
 
 @Configuration
@@ -49,13 +58,21 @@ class RelyingPartyConfiguration(
                 Saml2X509Credential.decryption(rsa, certificate)
 
             registrations.add(
-                createRegistration(
-                    applicationProperties.getSecurity().getSuomifi().samlSuomifiMetadataLocation!!,
-                    signingCredential,
-                    decryptionCredential,
-                    "suomifi",
-                    applicationProperties.getSecurity().getSuomifi().samlSuomifiEntityId!!
-                )
+                RelyingPartyRegistrations
+                    .fromMetadataLocation(
+                        applicationProperties.getSecurity()
+                            .getSuomifi().samlSuomifiMetadataLocation!!
+                    )
+                    .registrationId("suomifi")
+                    .assertingPartyDetails { party: RelyingPartyRegistration.AssertingPartyDetails.Builder ->
+                        party.entityId(
+                            applicationProperties.getSecurity().getSuomifi().samlSuomifiEntityId!!
+                        )
+                    }
+                    .signingX509Credentials { signing -> signing.add(signingCredential) }
+                    .decryptionX509Credentials { decryption -> decryption.add(decryptionCredential) }
+                    .singleLogoutServiceBinding(Saml2MessageBinding.REDIRECT)
+                    .build()
             )
         }
 
@@ -82,13 +99,20 @@ class RelyingPartyConfiguration(
 
             yliopistoRepository.findAllHaka().forEach {
                 registrations.add(
-                    createRegistration(
-                        applicationProperties.getSecurity().getHaka().samlHakaMetadataLocation!!,
-                        signingCredential,
-                        decryptionCredential,
-                        it.hakaId!!,
-                        it.hakaEntityId!!
-                    )
+                    RelyingPartyRegistrations
+                        .fromMetadata(ByteArrayInputStream(parseHakaFile(it.hakaEntityId!!).toByteArray()))
+                        .registrationId(it.hakaId!!)
+                        .assertingPartyDetails { party: RelyingPartyRegistration.AssertingPartyDetails.Builder ->
+                            party.entityId(it.hakaEntityId!!)
+                        }
+                        .signingX509Credentials { signing -> signing.add(signingCredential) }
+                        .decryptionX509Credentials { decryption ->
+                            decryption.add(
+                                decryptionCredential
+                            )
+                        }
+                        .singleLogoutServiceBinding(Saml2MessageBinding.REDIRECT)
+                        .build()
                 )
             }
         }
@@ -96,22 +120,52 @@ class RelyingPartyConfiguration(
         return if (registrations.size > 0) InMemoryRelyingPartyRegistrationRepository(registrations) else null
     }
 
-    private fun createRegistration(
-        metadataLocation: String,
-        signingCredential: Saml2X509Credential,
-        decryptionCredential: Saml2X509Credential,
-        registrationId: String,
-        entityId: String
-    ): RelyingPartyRegistration {
-        return RelyingPartyRegistrations
-            .fromMetadataLocation(metadataLocation)
-            .registrationId(registrationId)
-            .assertingPartyDetails { party: RelyingPartyRegistration.AssertingPartyDetails.Builder ->
-                party.entityId(entityId)
+    private fun parseHakaFile(entityId: String): String {
+        val url = URL(applicationProperties.getSecurity().getHaka().samlHakaMetadataLocation!!)
+        val stream: InputStream = url.openStream()
+
+        val inputFactory = XMLInputFactory.newInstance()
+        val outputFactory = XMLOutputFactory.newInstance()
+        val reader = inputFactory.createXMLEventReader(stream)
+        var writer: XMLEventWriter? = null
+        var writeElement = false
+        val outputStream = ByteArrayOutputStream()
+
+        while (reader.hasNext()) {
+            val event = reader.nextEvent()
+
+            when (event.eventType) {
+                START_ELEMENT -> {
+                    val startEvent = event as StartElement
+                    if (startEvent.name.localPart.equals("EntitiesDescriptor")) {
+                        writer = outputFactory.createXMLEventWriter(outputStream)
+                        writer?.add(event)
+                    } else if (startEvent.name.localPart.equals("EntityDescriptor")) {
+                        startEvent.attributes.forEach {
+                            if (it.name.localPart == "entityID" && it.value == entityId) {
+                                writeElement = true
+                                writer?.add(event)
+                            }
+                        }
+                    } else if (writeElement) writer?.add(event)
+                }
+                END_ELEMENT -> {
+                    val endEvent = event as EndElement
+                    if (endEvent.name.localPart.equals("EntityDescriptor") && writeElement) {
+                        writeElement = false
+                        writer?.add(event)
+                    } else if (endEvent.name.localPart.equals("EntitiesDescriptor")) {
+                        writer?.add(event)
+                        writer?.close()
+                        writer = null
+                    } else if (writeElement) writer?.add(event)
+                }
+                else -> if (writeElement) writer?.add(event)
             }
-            .signingX509Credentials { signing -> signing.add(signingCredential) }
-            .decryptionX509Credentials { decryption -> decryption.add(decryptionCredential) }
-            .singleLogoutServiceBinding(Saml2MessageBinding.REDIRECT)
-            .build()
+        }
+        reader.close()
+        writer?.close()
+        stream.close()
+        return outputStream.toString()
     }
 }
