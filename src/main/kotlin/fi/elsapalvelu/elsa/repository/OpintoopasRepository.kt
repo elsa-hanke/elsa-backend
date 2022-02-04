55c4e4a84a028ccc85e2794ce5a3c818ed8904b4
tviljaka <81751595+tviljaka@users.noreply.github.com>
2022-02-04 14:03:43 +0200

    UOELSA-923: Hae kaikki opinto-oppaat
    - Hae kaikki opinto-oppaat uutta erikoistujaa lisättäessä eikä vain niitä mitkä ovat voimassa nykyhetkenä

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/OpintoopasRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/OpintoopasRepository.kt
@@ -2,9 +2,7 @@ package fi.elsapalvelu.elsa.repository
 
 import fi.elsapalvelu.elsa.domain.Opintoopas
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
-import java.time.LocalDate
 
 
 @Repository
@@ -12,10 +10,4 @@ interface OpintoopasRepository : JpaRepository<Opintoopas, Long> {
 
     fun findAllByOpintooikeudetErikoistuvaLaakariKayttajaUserId(userId: String): List<Opintoopas>
 
-    @Query(
-        "select o from Opintoopas o " +
-            "where o.voimassaoloAlkaa <= ?1 " +
-            "and (o.voimassaoloPaattyy is null or o.voimassaoloPaattyy >= ?1)"
-    )
-    fun findAllByValid(valid: LocalDate): List<Opintoopas>
 }
