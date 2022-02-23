3deab7efc26efdc76159a52178712c2a525d9542
Arttu Hänninen <arttu.hanninen@gofore.com>
2022-02-23 16:03:47 +0200

    UOELSA-1032: Integraatiotestit

--- src/main/kotlin/fi/elsapalvelu/elsa/domain/ApplicationSetting.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/domain/ApplicationSetting.kt
@@ -3,6 +3,7 @@ package fi.elsapalvelu.elsa.domain
 import fi.elsapalvelu.elsa.domain.enumeration.ApplicationSettingTyyppi
 import org.hibernate.annotations.Cache
 import org.hibernate.annotations.CacheConcurrencyStrategy
+import java.io.Serializable
 import java.time.Instant
 import javax.persistence.*
 import javax.validation.constraints.NotNull
@@ -10,21 +11,35 @@ import javax.validation.constraints.NotNull
 @Entity
 @Table(name = "application_setting")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-class ApplicationSetting {
+data class ApplicationSetting(
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
+    var id: Long? = null,
 
-    @get: NotNull
+    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(name = "setting_name", nullable = false)
-    var settingName: ApplicationSettingTyyppi? = null
+    var settingName: ApplicationSettingTyyppi? = null,
 
     @Column(name = "boolean_setting", nullable = true)
-    var booleanSetting: Boolean? = null
+    var booleanSetting: Boolean? = null,
 
     @Column(name = "datetime_setting", nullable = true)
     var datetimeSetting: Instant? = null
 
+) : Serializable {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ApplicationSetting) return false
+
+        return id != null && other.id != null && id == other.id
+    }
+
+    override fun hashCode() = 31
+
+    override fun toString() = "ApplicationSetting{" +
+        "id=$id" +
+        "nimi=$settingName" +
+        "}"
 }
