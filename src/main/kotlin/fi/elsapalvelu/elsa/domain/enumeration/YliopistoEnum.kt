d915dc1745b12798355fc3d8ded9eb703c3a2c0d
tviljaka <81751595+tviljaka@users.noreply.github.com>
2022-02-23 18:54:56 +0200

    UOELSA-1006: Päivitä yliopistojen nimet const valueiksi

---
+++ src/main/kotlin/fi/elsapalvelu/elsa/domain/enumeration/YliopistoEnum.kt
@@ -0,0 +1,9 @@
+package fi.elsapalvelu.elsa.domain.enumeration
+
+enum class YliopistoEnum {
+    HELSINGIN_YLIOPISTO,
+    TAMPEREEN_YLIOPISTO,
+    TURUN_YLIOPISTO,
+    OULUN_YLIOPISTO,
+    ITA_SUOMEN_YLIOPISTO
+}
