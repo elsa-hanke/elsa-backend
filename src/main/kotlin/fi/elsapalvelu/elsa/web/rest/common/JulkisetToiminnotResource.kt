271bac67cbc9969c2d007afaf13fbaf0e6ac93fc
Arttu Hänninen <arttu.hanninen@gofore.com>
2022-02-23 15:36:41 +0200

    UOELSA-1032: Näytetään päivityksen aika vain, jos seuraavaan päivitykseen on alle 3 päivää aikaa

--- src/main/kotlin/fi/elsapalvelu/elsa/web/rest/common/JulkisetToiminnotResource.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/web/rest/common/JulkisetToiminnotResource.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.security.Principal
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 @RestController
 @RequestMapping("/api/julkinen/")
@@ -17,10 +18,21 @@ class JulkisetToiminnotResource(private val applicationSettingService: Applicati
     fun getSeuraavaPaivitys(
         principal: Principal?
     ): ResponseEntity<Instant> {
-        return ResponseEntity.ok(
-            applicationSettingService.getDatetimeSettingValue(
-                ApplicationSettingTyyppi.SEURAAVAN_PAIVITYKSEN_AIKA
+        val paivitysAika = applicationSettingService.getDatetimeSettingValue(
+            ApplicationSettingTyyppi.SEURAAVAN_PAIVITYKSEN_AIKA
+        )
+        val now = Instant.now()
+        if (paivitysAika != null && paivitysAika.isAfter(now) && ChronoUnit.DAYS.between(
+                now,
+                paivitysAika
+            ) < 3
+        ) {
+            return ResponseEntity.ok(
+                paivitysAika
             )
+        }
+        return ResponseEntity.ok(
+            null
         )
     }
 }
