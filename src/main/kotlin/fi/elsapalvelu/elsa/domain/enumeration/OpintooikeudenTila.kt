e3af5b7e4b594b0186a492da6cdb4b1b033f1a79
tviljaka <81751595+tviljaka@users.noreply.github.com>
2022-02-24 13:23:26 +0200

    UOELSA-1006: Muokkaa Opintooikeus entiteettiä
    - Lisää tila, muokkausaika ja yliopistoOpintooikeusId

---
+++ src/main/kotlin/fi/elsapalvelu/elsa/domain/enumeration/OpintooikeudenTila.kt
@@ -0,0 +1,10 @@
+package fi.elsapalvelu.elsa.domain.enumeration
+
+enum class OpintooikeudenTila {
+    AKTIIVINEN,
+    AKTIIVINEN_EI_LASNA,
+    PASSIIVINEN,
+    VALMISTUNUT,
+    PERUUTETTU,
+    VANHENTUNUT
+}
