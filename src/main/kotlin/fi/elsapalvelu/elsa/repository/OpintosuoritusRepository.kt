0ee122c954f5bf528f88f047c81d40464dffddf9
tviljaka <81751595+tviljaka@users.noreply.github.com>
2022-03-25 17:21:29 +0200

    UOELSA-1065: Lisää service, joka tallentaa opintosuoritteet kantaan

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/OpintosuoritusRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/OpintosuoritusRepository.kt
@@ -3,4 +3,10 @@ package fi.elsapalvelu.elsa.repository
 import fi.elsapalvelu.elsa.domain.Opintosuoritus
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface OpintosuoritusRepository : JpaRepository<Opintosuoritus, Long>
+interface OpintosuoritusRepository : JpaRepository<Opintosuoritus, Long> {
+
+    fun findOneByOpintooikeusYliopistoOpintooikeusIdAndKurssikoodi(
+        yliopistoOpintooikeusId: String,
+        kurssikoodi: String
+    ): Opintosuoritus?
+}
