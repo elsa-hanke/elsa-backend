b40bc45cb52baaac18cbb1b511df6812a24fff78
Arttu Hänninen <arttu.hanninen@gofore.com>
2022-04-01 15:07:31 +0300

    UOELSA-626: Luodaan hyväksytystä koulutussopimuksesta pdf

--- src/main/kotlin/fi/elsapalvelu/elsa/config/ThymeleafConfiguration.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/config/ThymeleafConfiguration.kt
@@ -2,8 +2,9 @@ package fi.elsapalvelu.elsa.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect
+import org.thymeleaf.spring5.SpringTemplateEngine
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
-
 import org.thymeleaf.templateresolver.ITemplateResolver
 
 
@@ -18,5 +19,13 @@ class ThymeleafConfiguration {
         templateResolver.setTemplateMode("HTML")
         templateResolver.characterEncoding = "UTF-8"
         return templateResolver
+    }
+
+    @Bean
+    fun templateEngine(): SpringTemplateEngine {
+        val engine = SpringTemplateEngine()
+        engine.addDialect(Java8TimeDialect())
+        engine.setTemplateResolver(thymeleafTemplateResolver())
+        return engine
     }
 }
