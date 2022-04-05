d3c687e48171ec7af256ebad60471874d923acb1
Arttu Hänninen <arttu.hanninen@gofore.com>
2022-04-05 17:00:34 +0300

    UOELSA-1097: Koejakson kehittämistoimenpiteiden osallisten sen hetkisiä tietoja ei tarvitse tallentaa kantaan ja erikoistuja ei allekirjoita vaihetta

--- src/main/kotlin/fi/elsapalvelu/elsa/domain/KoejaksonKehittamistoimenpiteet.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/domain/KoejaksonKehittamistoimenpiteet.kt
@@ -26,22 +26,6 @@ data class KoejaksonKehittamistoimenpiteet(
     @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
     var opintooikeus: Opintooikeus? = null,
 
-    @NotNull
-    @Column(name = "erikoistuvan_nimi", nullable = false)
-    var erikoistuvanNimi: String? = null,
-
-    @NotNull
-    @Column(name = "erikoistuvan_erikoisala", nullable = false)
-    var erikoistuvanErikoisala: String? = null,
-
-    @NotNull
-    @Column(name = "erikoistuvan_opiskelijatunnus", nullable = false)
-    var erikoistuvanOpiskelijatunnus: String? = null,
-
-    @NotNull
-    @Column(name = "erikoistuvan_yliopisto", nullable = false)
-    var erikoistuvanYliopisto: String? = null,
-
     @Column(name = "kehittamistoimenpiteet_riittavat")
     var kehittamistoimenpiteetRiittavat: Boolean? = null,
 
@@ -49,10 +33,6 @@ data class KoejaksonKehittamistoimenpiteet(
     @ManyToOne(optional = false)
     @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
     var lahikouluttaja: Kayttaja? = null,
-
-    @NotNull
-    @Column(name = "lahikouluttajan_nimi", nullable = false)
-    var lahikouluttajanNimi: String? = null,
 
     @Column(name = "lahikouluttaja_hyvaksynyt")
     var lahikouluttajaHyvaksynyt: Boolean = false,
@@ -64,10 +44,6 @@ data class KoejaksonKehittamistoimenpiteet(
     @ManyToOne(optional = false)
     @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
     var lahiesimies: Kayttaja? = null,
-
-    @NotNull
-    @Column(name = "lahiesimiehen_nimi", nullable = false)
-    var lahiesimiehenNimi: String? = null,
 
     @Column(name = "lahiesimies_hyvaksynyt")
     var lahiesimiesHyvaksynyt: Boolean = false,
@@ -82,11 +58,8 @@ data class KoejaksonKehittamistoimenpiteet(
     @Column(name = "korjausehdotus")
     var korjausehdotus: String? = null,
 
-    @Column(name = "erikoistuva_allekirjoittanut")
-    var erikoistuvaAllekirjoittanut: Boolean = false,
-
-    @Column(name = "erikoistuvan_allekirjoitusaika", nullable = false)
-    var erikoistuvanAllekirjoitusaika: LocalDate? = null
+    @Column(name = "erikoistuvan_kuittausaika", nullable = false)
+    var erikoistuvanKuittausaika: LocalDate? = null
 
 ) : Serializable {
 
@@ -113,20 +86,13 @@ data class KoejaksonKehittamistoimenpiteet(
         return "KoejaksonKehittamistoimenpiteet(" +
             "id=$id, " +
             "opintooikeus=$opintooikeus, " +
-            "erikoistuvanNimi=$erikoistuvanNimi, " +
-            "erikoistuvanErikoisala=$erikoistuvanErikoisala, " +
-            "erikoistuvanOpiskelijatunnus=$erikoistuvanOpiskelijatunnus, " +
-            "erikoistuvanYliopisto=$erikoistuvanYliopisto, " +
             "kehittamistoimenpiteetRiittavat=$kehittamistoimenpiteetRiittavat, " +
             "lahikouluttaja=$lahikouluttaja, " +
-            "lahikouluttajanNimi=$lahikouluttajanNimi, " +
             "lahikouluttajaHyvaksynyt=$lahikouluttajaHyvaksynyt, " +
             "lahikouluttajanKuittausaika=$lahikouluttajanKuittausaika, " +
-            "lahiesimies=$lahiesimies, lahiesimiehenNimi=$lahiesimiehenNimi, " +
             "lahiesimiesHyvaksynyt=$lahiesimiesHyvaksynyt, " +
             "lahiesimiehenKuittausaika=$lahiesimiehenKuittausaika, " +
-            "muokkauspaiva=$muokkauspaiva, korjausehdotus=$korjausehdotus, " +
-            "erikoistuvaAllekirjoittanut=$erikoistuvaAllekirjoittanut)"
+            "muokkauspaiva=$muokkauspaiva, korjausehdotus=$korjausehdotus)"
     }
 
     companion object {
