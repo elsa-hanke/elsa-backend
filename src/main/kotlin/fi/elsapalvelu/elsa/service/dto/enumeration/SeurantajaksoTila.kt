9aa44a0983869cb5af463ae7621c9d077f6241e1
Arttu Hänninen <arttu.hanninen@gofore.com>
2022-07-05 12:50:01 +0300

    UOELSA-874: Seurantajaksot kouluttajan etusivulle

--- src/main/kotlin/fi/elsapalvelu/elsa/service/dto/enumeration/SeurantajaksoTila.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/dto/enumeration/SeurantajaksoTila.kt
@@ -1,5 +1,6 @@
 package fi.elsapalvelu.elsa.service.dto.enumeration
 
+import fi.elsapalvelu.elsa.domain.Seurantajakso
 import fi.elsapalvelu.elsa.service.dto.*
 
 enum class SeurantajaksoTila {
@@ -12,11 +13,34 @@ enum class SeurantajaksoTila {
 
     companion object {
         fun fromSeurantajakso(seurantajaksoDTO: SeurantajaksoDTO): SeurantajaksoTila {
-            return if (seurantajaksoDTO.hyvaksytty == true) HYVAKSYTTY
-            else if (!seurantajaksoDTO.korjausehdotus.isNullOrBlank()) PALAUTETTU_KORJATTAVAKSI
-            else if (seurantajaksoDTO.kouluttajanArvio == null && seurantajaksoDTO.seurantakeskustelunYhteisetMerkinnat == null) ODOTTAA_ARVIOINTIA_JA_YHTEISIA_MERKINTOJA
-            else if (seurantajaksoDTO.kouluttajanArvio == null) ODOTTAA_ARVIOINTIA
-            else if (seurantajaksoDTO.seurantakeskustelunYhteisetMerkinnat == null) ODOTTAA_YHTEISIA_MERKINTOJA
+            return haeTila(
+                seurantajaksoDTO.hyvaksytty,
+                seurantajaksoDTO.korjausehdotus,
+                seurantajaksoDTO.kouluttajanArvio,
+                seurantajaksoDTO.seurantakeskustelunYhteisetMerkinnat
+            )
+        }
+
+        fun fromSeurantajakso(seurantajakso: Seurantajakso): SeurantajaksoTila {
+            return haeTila(
+                seurantajakso.hyvaksytty,
+                seurantajakso.korjausehdotus,
+                seurantajakso.kouluttajanArvio,
+                seurantajakso.seurantakeskustelunYhteisetMerkinnat
+            )
+        }
+
+        private fun haeTila(
+            hyvaksytty: Boolean?,
+            korjausehdotus: String?,
+            kouluttajanArvio: String?,
+            seurantakeskustelunYhteisetMerkinnat: String?
+        ): SeurantajaksoTila {
+            return if (hyvaksytty == true) HYVAKSYTTY
+            else if (!korjausehdotus.isNullOrBlank()) PALAUTETTU_KORJATTAVAKSI
+            else if (kouluttajanArvio == null && seurantakeskustelunYhteisetMerkinnat == null) ODOTTAA_ARVIOINTIA_JA_YHTEISIA_MERKINTOJA
+            else if (kouluttajanArvio == null) ODOTTAA_ARVIOINTIA
+            else if (seurantakeskustelunYhteisetMerkinnat == null) ODOTTAA_YHTEISIA_MERKINTOJA
             else ODOTTAA_HYVAKSYNTAA
         }
     }
