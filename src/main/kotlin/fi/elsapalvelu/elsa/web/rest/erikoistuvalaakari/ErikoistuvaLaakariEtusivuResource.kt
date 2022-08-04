5f2b04c4e2b98f7df1d46bafe08ea5b5d2319882
tviljaka <81751595+tviljaka@users.noreply.github.com>
2022-08-04 15:51:28 +0300

    UOELSA-979: Toteuta kouluttajavaltuutuksen päättymisen indikointi

--- src/main/kotlin/fi/elsapalvelu/elsa/web/rest/erikoistuvalaakari/ErikoistuvaLaakariEtusivuResource.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/web/rest/erikoistuvalaakari/ErikoistuvaLaakariEtusivuResource.kt
@@ -2,6 +2,7 @@ package fi.elsapalvelu.elsa.web.rest.erikoistuvalaakari
 
 import fi.elsapalvelu.elsa.service.EtusivuService
 import fi.elsapalvelu.elsa.service.UserService
+import fi.elsapalvelu.elsa.service.dto.AvoinAsiaDTO
 import fi.elsapalvelu.elsa.service.dto.ErikoistumisenEdistyminenDTO
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,6 +16,14 @@ class ErikoistuvaLaakariEtusivuResource(
     private val userService: UserService,
     private val etusivuService: EtusivuService
 ) {
+
+    @GetMapping("/avoimet-asiat")
+    fun getAvoimetAsiat(
+        principal: Principal?
+    ): ResponseEntity<List<AvoinAsiaDTO>> {
+        val user = userService.getAuthenticatedUser(principal)
+        return ResponseEntity.ok(etusivuService.getAvoimetAsiatForErikoistuja(user.id!!))
+    }
 
     @GetMapping("/erikoistumisen-edistyminen")
     fun getErikoistumisenEdistyminen(
