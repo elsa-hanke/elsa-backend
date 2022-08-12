45c57331becb4de0975870ac1314763178dcaf41
tviljaka <81751595+tviljaka@users.noreply.github.com>
2022-08-12 16:36:22 +0300

    UOELSA-1165: Lisää erikoisalaId OpintoopasSimpleDTO:lle

--- src/main/kotlin/fi/elsapalvelu/elsa/service/mapper/OpintoopasSimpleMapper.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/mapper/OpintoopasSimpleMapper.kt
@@ -3,6 +3,7 @@ package fi.elsapalvelu.elsa.service.mapper
 import fi.elsapalvelu.elsa.domain.Opintoopas
 import fi.elsapalvelu.elsa.service.dto.OpintoopasSimpleDTO
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import org.mapstruct.Mappings
 import org.mapstruct.ReportingPolicy
 
@@ -13,7 +14,9 @@ import org.mapstruct.ReportingPolicy
 )
 interface OpintoopasSimpleMapper : EntityMapper<OpintoopasSimpleDTO, Opintoopas> {
 
-    @Mappings()
+    @Mappings(
+        Mapping(source = "erikoisala.id", target = "erikoisalaId"),
+    )
     override fun toDto(entity: Opintoopas): OpintoopasSimpleDTO
 
     @Mappings()
