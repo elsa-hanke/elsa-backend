6cded4a5650f5094f1494f328fc8f9fefd5ec22d
Arttu Hänninen <arttu.hanninen@gofore.com>
2022-09-09 11:34:02 +0300

    UOELSA-834: Terveyskeskuskoulutusjakson virkailijan tarkistuksen tietomallimuutokset

--- src/main/kotlin/fi/elsapalvelu/elsa/domain/TerveyskeskuskoulutusjaksonHyvaksynta.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/domain/TerveyskeskuskoulutusjaksonHyvaksynta.kt
@@ -5,6 +5,8 @@ import org.hibernate.annotations.CacheConcurrencyStrategy
 import org.hibernate.envers.Audited
 import org.hibernate.envers.RelationTargetAuditMode
 import java.io.Serializable
+import java.time.LocalDate
+import java.time.ZoneId
 import javax.persistence.*
 import javax.validation.constraints.NotNull
 
@@ -29,12 +31,40 @@ data class TerveyskeskuskoulutusjaksonHyvaksynta(
     @Column(name = "korjausehdotus")
     var korjausehdotus: String? = null,
 
+    @Column(name = "korjausehdotus_vastuuhenkilolta")
+    var korjausehdotusVastuuhenkilolta: String? = null,
+
+    @Column(name = "lisatiedot_virkailijalta")
+    var lisatiedotVirkailijalta: String? = null,
+
     @OneToOne(cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.EAGER)
     @JoinColumn(unique = true)
     @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
-    var opintooikeus: Opintooikeus? = null
+    var opintooikeus: Opintooikeus? = null,
+
+    @ManyToOne
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    var virkailija: Kayttaja? = null,
+
+    @ManyToOne
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    var vastuuhenkilo: Kayttaja? = null,
+
+    @NotNull
+    @Column(name = "muokkauspaiva", nullable = false)
+    var muokkauspaiva: LocalDate? = null
 
 ) : Serializable {
+
+    @PrePersist
+    protected fun onCreate() {
+        muokkauspaiva = LocalDate.now(ZoneId.systemDefault())
+    }
+
+    @PreUpdate
+    protected fun onUpdate() {
+        muokkauspaiva = LocalDate.now(ZoneId.systemDefault())
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
