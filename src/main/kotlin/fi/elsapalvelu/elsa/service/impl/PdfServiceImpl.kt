35773c9f367afe759e27b7580987f7dcb8bf8b83
Arttu Hänninen <arttu.hanninen@gofore.com>
2022-11-10 16:42:06 +0200

    UOELSA-842: Lisätään kuvat yhdistettyyn pdf tiedostoon

--- src/main/kotlin/fi/elsapalvelu/elsa/service/impl/PdfServiceImpl.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/impl/PdfServiceImpl.kt
@@ -2,21 +2,25 @@ package fi.elsapalvelu.elsa.service.impl
 
 import com.itextpdf.html2pdf.ConverterProperties
 import com.itextpdf.html2pdf.HtmlConverter
+import com.itextpdf.io.image.ImageDataFactory
 import com.itextpdf.kernel.pdf.*
 import com.itextpdf.kernel.utils.PdfMerger
+import com.itextpdf.layout.Document
+import com.itextpdf.layout.element.Image
 import com.itextpdf.layout.font.FontProvider
+import com.itextpdf.layout.properties.ObjectFit
+import com.itextpdf.layout.properties.UnitValue
 import com.itextpdf.pdfa.PdfADocument
+import fi.elsapalvelu.elsa.domain.Asiakirja
 import fi.elsapalvelu.elsa.service.PdfService
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import org.thymeleaf.context.Context
 import org.thymeleaf.spring5.SpringTemplateEngine
 import java.io.ByteArrayOutputStream
 import java.io.FileInputStream
-import java.sql.Blob
 
 @Service
-@Transactional
 class PdfServiceImpl(
     private val templateEngine: SpringTemplateEngine
 ) : PdfService {
@@ -41,13 +45,27 @@ class PdfServiceImpl(
         HtmlConverter.convertToPdf(content, pdf, properties)
     }
 
-    override fun yhdistaAsiakirjat(asiakirjat: List<Blob?>, outputStream: ByteArrayOutputStream) {
+    override fun yhdistaAsiakirjat(
+        asiakirjat: List<Asiakirja>,
+        outputStream: ByteArrayOutputStream
+    ) {
         val result = PdfDocument(PdfWriter(outputStream))
+        val resultDocument = Document(result)
         val merger = PdfMerger(result)
-        asiakirjat.forEach {
-            val document = PdfDocument(PdfReader(it?.binaryStream))
+        asiakirjat.filter { it.tyyppi == MediaType.APPLICATION_PDF_VALUE }.forEach {
+            val document = PdfDocument(PdfReader(it.asiakirjaData?.data?.binaryStream))
             merger.merge(document, 1, document.numberOfPages)
         }
-        result.close()
+        asiakirjat.filter { it.tyyppi == MediaType.IMAGE_JPEG_VALUE || it.tyyppi == MediaType.IMAGE_PNG_VALUE }
+            .forEach {
+                result.addNewPage()
+                val image =
+                    Image(ImageDataFactory.create(it.asiakirjaData?.data?.binaryStream?.readAllBytes()))
+                image.width = UnitValue(1, result.getPage(result.numberOfPages).pageSize.width)
+                image.setFixedPosition(result.numberOfPages, 0F, 0F)
+                image.objectFit = ObjectFit.SCALE_DOWN
+                resultDocument.add(image)
+            }
+        resultDocument.close()
     }
 }
