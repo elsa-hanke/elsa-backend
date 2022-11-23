ced07be311225974fafa3dd47835f02987ee65e0
Arttu Hänninen <arttu.hanninen@gofore.com>
2022-11-23 10:24:38 +0200

    UOELSA-1183: Arvioitavien kokonaisuuksien kuvauksien haku sivutettuna

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/ArvioitavaKokonaisuusRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/ArvioitavaKokonaisuusRepository.kt
@@ -1,6 +1,8 @@
 package fi.elsapalvelu.elsa.repository
 
 import fi.elsapalvelu.elsa.domain.ArvioitavaKokonaisuus
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
@@ -17,5 +19,18 @@ interface ArvioitavaKokonaisuusRepository : JpaRepository<ArvioitavaKokonaisuus,
     )
     fun findAllByErikoisalaIdAndValid(id: Long?, valid: LocalDate): List<ArvioitavaKokonaisuus>
 
-    fun findAllByKategoriaErikoisalaIdIn(erikoisalaIds: List<Long>): List<ArvioitavaKokonaisuus>
+    @Query(
+        "select oa from ArvioitavaKokonaisuus oa join oa.kategoria k left join k.erikoisala e " +
+            "where e.id = ?1 " +
+            "and oa.voimassaoloAlkaa <= ?2 " +
+            "and (oa.voimassaoloLoppuu is null or oa.voimassaoloLoppuu >= ?2)"
+    )
+    fun findAllByErikoisalaIdAndValid(id: Long?, valid: LocalDate, pageable: Pageable): Page<ArvioitavaKokonaisuus>
+
+    @Query(
+        "select oa from ArvioitavaKokonaisuus oa join oa.kategoria k left join k.erikoisala e " +
+            "where e.id = ?1 " +
+            "and oa.voimassaoloLoppuu < ?2"
+    )
+    fun findAllByErikoisalaIdAndExpired(id: Long?, valid: LocalDate, pageable: Pageable): Page<ArvioitavaKokonaisuus>
 }
