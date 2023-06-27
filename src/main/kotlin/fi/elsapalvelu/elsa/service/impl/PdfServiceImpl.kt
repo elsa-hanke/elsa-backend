b6555ad0a52e0ae83cfaf64ac6ce35e058e8fe54
Arttu Hänninen <arttu.hanninen@gofore.com>
2023-06-27 16:21:53 +0300

    UOELSA-1314: Korruptoituneet tiedostot eivät saa kaataa pdf:n generointia

--- src/main/kotlin/fi/elsapalvelu/elsa/service/impl/PdfServiceImpl.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/impl/PdfServiceImpl.kt
@@ -2,6 +2,7 @@ package fi.elsapalvelu.elsa.service.impl
 
 import com.itextpdf.html2pdf.ConverterProperties
 import com.itextpdf.html2pdf.HtmlConverter
+import com.itextpdf.io.exceptions.IOException
 import com.itextpdf.io.image.ImageDataFactory
 import com.itextpdf.kernel.pdf.*
 import com.itextpdf.kernel.utils.PdfMerger
@@ -13,6 +14,7 @@ import com.itextpdf.layout.properties.UnitValue
 import com.itextpdf.pdfa.PdfADocument
 import fi.elsapalvelu.elsa.domain.Asiakirja
 import fi.elsapalvelu.elsa.service.PdfService
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.Resource
 import org.springframework.http.MediaType
@@ -25,6 +27,8 @@ import java.io.*
 class PdfServiceImpl(
     private val templateEngine: SpringTemplateEngine
 ) : PdfService {
+
+    private val log = LoggerFactory.getLogger(javaClass)
 
     @Value("classpath:sRGB_CS_profile.icm")
     var colorProfile: Resource? = null
@@ -68,12 +72,16 @@ class PdfServiceImpl(
         }
         asiakirjat.filter { it.tyyppi == MediaType.IMAGE_JPEG_VALUE || it.tyyppi == MediaType.IMAGE_PNG_VALUE }
             .forEach {
-                result.addNewPage()
-                val image = Image(ImageDataFactory.create(it.asiakirjaData?.data))
-                image.width = UnitValue(1, result.getPage(result.numberOfPages).pageSize.width)
-                image.setFixedPosition(result.numberOfPages, 0F, 0F)
-                image.objectFit = ObjectFit.SCALE_DOWN
-                resultDocument.add(image)
+                try {
+                    val image = Image(ImageDataFactory.create(it.asiakirjaData?.data))
+                    result.addNewPage()
+                    image.width = UnitValue(1, result.getPage(result.numberOfPages).pageSize.width)
+                    image.setFixedPosition(result.numberOfPages, 0F, 0F)
+                    image.objectFit = ObjectFit.SCALE_DOWN
+                    resultDocument.add(image)
+                } catch (e: IOException) {
+                    log.warn("Asiakirjan ${it.id} lisäys epäonnistui", e)
+                }
             }
         resultDocument.close()
     }
