73fbc25b63e05f9ad80868a6c63cd02f8a4ead7d
Martin Kankaanranta <martin.kankaanranta@gofore.com>
2023-11-23 10:01:40 +0200

    ELSA-328: muuta opintosuoritus hakua osakokonaisuuksiin liittyen

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/OpintosuoritusRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/OpintosuoritusRepository.kt
@@ -16,8 +16,10 @@ interface OpintosuoritusRepository : JpaRepository<Opintosuoritus, Long> {
             select distinct o from Opintosuoritus o left join fetch o.osakokonaisuudet
             where o.opintooikeus.id = :opintooikeusId and o.kurssikoodi not in
             (select oo.kurssikoodi from Opintosuoritus o join o.osakokonaisuudet oo
-            where o.opintooikeus.id = :opintooikeusId)
+            join OpintosuoritusKurssikoodi kk on kk.tunniste = oo.kurssikoodi
+            where o.opintooikeus.id = :opintooikeusId and kk.isOsakokonaisuus = true)
         """
     )
     fun findAllByOpintooikeusId(opintooikeusId: Long): List<Opintosuoritus>
+
 }
