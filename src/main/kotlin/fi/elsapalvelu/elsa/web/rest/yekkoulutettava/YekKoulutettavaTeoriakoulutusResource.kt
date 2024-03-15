50d2c870215221c3c6fa6a19c5e250349a2ce6e8
Martin Kankaanranta <martin.kankaanranta@gofore.com>
2024-03-15 15:56:24 +0200

    Muutos hakuun ja testit

--- src/main/kotlin/fi/elsapalvelu/elsa/web/rest/yekkoulutettava/YekKoulutettavaTeoriakoulutusResource.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/web/rest/yekkoulutettava/YekKoulutettavaTeoriakoulutusResource.kt
@@ -1,10 +1,13 @@
 package fi.elsapalvelu.elsa.web.rest.yekkoulutettava
 
 import fi.elsapalvelu.elsa.config.YEK_ERIKOISALA_ID
+import fi.elsapalvelu.elsa.domain.enumeration.OpintosuoritusTyyppiEnum
 import fi.elsapalvelu.elsa.service.OpintooikeusService
 import fi.elsapalvelu.elsa.service.OpintosuoritusService
+import fi.elsapalvelu.elsa.service.OpintosuoritusTyyppiService
 import fi.elsapalvelu.elsa.service.UserService
 import fi.elsapalvelu.elsa.service.dto.OpintosuoritusDTO
+import fi.elsapalvelu.elsa.web.rest.errors.BadRequestAlertException
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,6 +18,7 @@ import java.security.Principal
 @RequestMapping("/api/yek-koulutettava")
 class YekKoulutettavaTeoriakoulutusResource(
     private val opintosuoritusService: OpintosuoritusService,
+    private val opintosuoritusTyyppiService: OpintosuoritusTyyppiService,
     private val userService: UserService,
     private val opintooikeusService: OpintooikeusService
 ) {
@@ -26,8 +30,27 @@ class YekKoulutettavaTeoriakoulutusResource(
         val user = userService.getAuthenticatedUser(principal)
         val opintooikeus =
             opintooikeusService.findOneByKaytossaAndErikoistuvaLaakariKayttajaUserIdAndErikoisalaId(user.id!!, YEK_ERIKOISALA_ID)
-        val opintosuorituksetDTO = opintosuoritusService.getOpintosuorituksetByOpintooikeusIdAndTyyppiId(opintooikeus.id!!, 6)
+        val opintosuoritusTyypit = opintosuoritusTyyppiService.findAll()
+        var tyyppiId: Long? = null
+
+        opintosuoritusTyypit?.forEach { opintosuoritusTyyppi ->
+            if (opintosuoritusTyyppi.nimi == OpintosuoritusTyyppiEnum.YLEISLAAKETIETEEN_ERITYISKOULUTUS) {
+                tyyppiId = opintosuoritusTyyppi.id
+                return@forEach
+            }
+        }
+
+        if (tyyppiId == null) {
+            throw BadRequestAlertException(
+                "Opintosuoritus tyyppiä", OpintosuoritusTyyppiEnum.YLEISLAAKETIETEEN_ERITYISKOULUTUS.toString(), "ei löytynyt"
+            )
+        }
+
+        val opintosuorituksetDTO = opintosuoritusService.getOpintosuorituksetByOpintooikeusIdAndTyyppiId(
+            opintooikeus.id!!, tyyppiId!!
+        )
         return ResponseEntity.ok(opintosuorituksetDTO.opintosuoritukset)
     }
+
 
 }
--- src/test/kotlin/fi/elsapalvelu/elsa/web/rest/yekkoulutettava/YekKoulutettavaTeoriakoulutusResourceIT.kt
+++ src/test/kotlin/fi/elsapalvelu/elsa/web/rest/yekkoulutettava/YekKoulutettavaTeoriakoulutusResourceIT.kt
@@ -1,32 +1,35 @@
 package fi.elsapalvelu.elsa.web.rest.yekkoulutettava
 
 import fi.elsapalvelu.elsa.ElsaBackendApp
-import fi.elsapalvelu.elsa.domain.Teoriakoulutus
+import fi.elsapalvelu.elsa.domain.ErikoistuvaLaakari
+import fi.elsapalvelu.elsa.domain.Opintosuoritus
 import fi.elsapalvelu.elsa.domain.User
-import fi.elsapalvelu.elsa.repository.TeoriakoulutusRepository
+import fi.elsapalvelu.elsa.domain.enumeration.OpintosuoritusTyyppiEnum
 import fi.elsapalvelu.elsa.security.YEK_KOULUTETTAVA
 import fi.elsapalvelu.elsa.web.rest.common.KayttajaResourceWithMockUserIT
-import fi.elsapalvelu.elsa.web.rest.erikoistuvalaakari.ErikoistuvaLaakariTeoriakoulutusResourceIT
+import fi.elsapalvelu.elsa.web.rest.findAll
+import fi.elsapalvelu.elsa.web.rest.helpers.*
 import jakarta.persistence.EntityManager
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.mockito.MockitoAnnotations
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.saml2.provider.service.authentication.DefaultSaml2AuthenticatedPrincipal
 import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication
 import org.springframework.security.test.context.TestSecurityContextHolder
 import org.springframework.test.web.servlet.MockMvc
-import java.time.LocalDate
-import java.time.ZoneId
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.transaction.annotation.Transactional
 
 @AutoConfigureMockMvc
 @SpringBootTest(classes = [ElsaBackendApp::class])
 class YekKoulutettavaTeoriakoulutusResourceIT {
-
-    @Autowired
-    private lateinit var teoriakoulutusRepository: TeoriakoulutusRepository
 
     @Autowired
     private lateinit var em: EntityManager
@@ -34,8 +37,7 @@ class YekKoulutettavaTeoriakoulutusResourceIT {
     @Autowired
     private lateinit var restTeoriakoulutusMockMvc: MockMvc
 
-    private lateinit var teoriakoulutus: Teoriakoulutus
-
+    private lateinit var opintosuoritus: Opintosuoritus
     private lateinit var user: User
 
     @BeforeEach
@@ -43,47 +45,28 @@ class YekKoulutettavaTeoriakoulutusResourceIT {
         MockitoAnnotations.openMocks(this)
     }
 
-    // @Test
-    // @Transactional
-    // @Throws(Exception::class)
-    // fun getAllTeoriakoulutukset() {
-    //     initTest()
-    //     val erikoisala = ErikoisalaHelper.createEntity()
-    //     em.persist(erikoisala)
-    //     em.flush()
-    //     teoriakoulutus.opintooikeus?.erikoisala = erikoisala
-    //     teoriakoulutusRepository.saveAndFlush(teoriakoulutus)
-//
-    //     restTeoriakoulutusMockMvc.perform(get(ENTITY_API_URL))
-    //         .andExpect(status().isOk)
-    //         .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-    //         .andExpect(jsonPath("$.teoriakoulutukset.[*].id").value(hasItem(teoriakoulutus.id?.toInt())))
-    //         .andExpect(jsonPath("$.teoriakoulutukset.[*].koulutuksenNimi").value(hasItem(DEFAULT_KOULUTUKSEN_NIMI)))
-    //         .andExpect(jsonPath("$.teoriakoulutukset.[*].koulutuksenPaikka").value(hasItem(DEFAULT_KOULUTUKSEN_PAIKKA)))
-    //         .andExpect(jsonPath("$.teoriakoulutukset.[*].alkamispaiva").value(hasItem(DEFAULT_ALKAMISPAIVA.toString())))
-    //         .andExpect(
-    //             jsonPath("$.teoriakoulutukset.[*].paattymispaiva").value(hasItem(DEFAULT_PAATTYMISPAIVA.toString()))
-    //         )
-    //         .andExpect(
-    //             jsonPath("$.teoriakoulutukset.[*].erikoistumiseenHyvaksyttavaTuntimaara").value(
-    //                 hasItem(
-    //                     DEFAULT_ERIKOISTUMISEEN_HYVAKSYTTAVA_TUNTIMAARA
-    //                 )
-    //             )
-    //         )
-    //         .andExpect(
-    //             jsonPath("$.erikoisalanVaatimaTeoriakoulutustenVahimmaismaara").value(
-    //                 DEFAULT_ERIKOISALAN_VAATIMA_TEORIAKOULUTUSTEN_VAHIMMAISMAARA
-    //             )
-    //         )
-    // }
+    @Test
+    @Transactional
+    @Throws(Exception::class)
+    fun getAllTeoriakoulutukset() {
+        initTest()
 
-    fun initTest() {
+        restTeoriakoulutusMockMvc.perform(get(ENTITY_API_URL))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$").value(Matchers.hasSize<Any>(1)))
+            .andExpect(jsonPath("$[0].id").value(opintosuoritus.id as Any))
+            .andExpect(jsonPath("$[0].nimi_fi").value(OpintosuoritusHelper.DEFAULT_NIMI_FI))
+            .andExpect(jsonPath("$[0].nimi_sv").value(OpintosuoritusHelper.DEFAULT_NIMI_SV))
+    }
+
+
+    fun initTest(userId: String? = KayttajaHelper.DEFAULT_ID) {
         user = KayttajaResourceWithMockUserIT.createEntity()
         em.persist(user)
         em.flush()
-        val userDetails = mapOf<String, List<Any>>(
-        )
+
+        val userDetails = mapOf<String, List<Any>>()
         val authorities = listOf(SimpleGrantedAuthority(YEK_KOULUTETTAVA))
         val authentication = Saml2Authentication(
             DefaultSaml2AuthenticatedPrincipal(user.id, userDetails),
@@ -92,72 +75,32 @@ class YekKoulutettavaTeoriakoulutusResourceIT {
         )
         TestSecurityContextHolder.getContext().authentication = authentication
 
-        teoriakoulutus = ErikoistuvaLaakariTeoriakoulutusResourceIT.createEntity(em, user)
+        // Lisätään pakollinen tieto
+        var erikoistuvaLaakari =
+            em.findAll(ErikoistuvaLaakari::class).firstOrNull { it.kayttaja?.user == user }
+        if (erikoistuvaLaakari == null) {
+            erikoistuvaLaakari = ErikoistuvaLaakariHelper.createEntity(em, user)
+            em.persist(erikoistuvaLaakari)
+            em.flush()
+        }
+
+        val opintooikeus = OpintooikeusHelper.addOpintooikeusForYekKoulutettava(em, erikoistuvaLaakari)
+        OpintooikeusHelper.setOpintooikeusKaytossa(erikoistuvaLaakari, opintooikeus)
+
+        val opintosuoritusTyyppi = OpintosuoritusTyyppiEnum.YLEISLAAKETIETEEN_ERITYISKOULUTUS
+
+        opintosuoritus = OpintosuoritusHelper.createEntity(
+            em,
+            user,
+            OPINTOSUORITUS1_KURSSIKOODI,
+            opintosuoritusTyyppi,
+            opintooikeus = opintooikeus
+        )
+        em.persist(opintosuoritus)
     }
 
     companion object {
-
-        private const val DEFAULT_KOULUTUKSEN_NIMI = "AAAAAAAAAA"
-        private const val UPDATED_KOULUTUKSEN_NIMI = "BBBBBBBBBB"
-
-        private const val DEFAULT_KOULUTUKSEN_PAIKKA = "AAAAAAAAAA"
-        private const val UPDATED_KOULUTUKSEN_PAIKKA = "BBBBBBBBBB"
-
-        private val DEFAULT_ALKAMISPAIVA: LocalDate = LocalDate.ofEpochDay(0L)
-        private val UPDATED_ALKAMISPAIVA: LocalDate = LocalDate.now(ZoneId.systemDefault())
-
-        private val DEFAULT_PAATTYMISPAIVA: LocalDate = LocalDate.ofEpochDay(0L)
-        private val UPDATED_PAATTYMISPAIVA: LocalDate = LocalDate.now(ZoneId.systemDefault())
-
-        private const val DEFAULT_ERIKOISTUMISEEN_HYVAKSYTTAVA_TUNTIMAARA: Double = 5.0
-        private const val UPDATED_ERIKOISTUMISEEN_HYVAKSYTTAVA_TUNTIMAARA: Double = 10.0
-
+        private const val OPINTOSUORITUS1_KURSSIKOODI = "YLEE0028"
         private val ENTITY_API_URL: String = "/api/yek-koulutettava/teoriakoulutukset"
-
-        // @JvmStatic
-        // fun createEntity(em: EntityManager, user: User? = null): Teoriakoulutus {
-        //     val teoriakoulutus = Teoriakoulutus(
-        //         koulutuksenNimi = DEFAULT_KOULUTUKSEN_NIMI,
-        //         koulutuksenPaikka = DEFAULT_KOULUTUKSEN_PAIKKA,
-        //         alkamispaiva = DEFAULT_ALKAMISPAIVA,
-        //         paattymispaiva = DEFAULT_PAATTYMISPAIVA,
-        //         erikoistumiseenHyvaksyttavaTuntimaara = DEFAULT_ERIKOISTUMISEEN_HYVAKSYTTAVA_TUNTIMAARA
-        //     )
-        //     // Lisätään pakollinen tieto
-        //     var erikoistuvaLaakari =
-        //         em.findAll(ErikoistuvaLaakari::class).firstOrNull { it.kayttaja?.user == user }
-        //     if (erikoistuvaLaakari == null) {
-        //         erikoistuvaLaakari = ErikoistuvaLaakariHelper.createEntity(em, user)
-        //         em.persist(erikoistuvaLaakari)
-        //         em.flush()
-        //     }
-        //     val newOpintooikeus =
-        //         OpintooikeusHelper.addOpintooikeusForYekKoulutettava(em, erikoistuvaLaakari)
-        //     OpintooikeusHelper.setOpintooikeusKaytossa(erikoistuvaLaakari, newOpintooikeus)
-        //     teoriakoulutus.opintooikeus = erikoistuvaLaakari.getOpintooikeusKaytossa()
-        //     return teoriakoulutus
-        // }
-
-        // @JvmStatic
-        // fun createUpdatedEntity(em: EntityManager): Teoriakoulutus {
-        //     val teoriakoulutus = Teoriakoulutus(
-        //         koulutuksenNimi = UPDATED_KOULUTUKSEN_NIMI,
-        //         koulutuksenPaikka = UPDATED_KOULUTUKSEN_PAIKKA,
-        //         alkamispaiva = UPDATED_ALKAMISPAIVA,
-        //         paattymispaiva = UPDATED_PAATTYMISPAIVA,
-        //         erikoistumiseenHyvaksyttavaTuntimaara = UPDATED_ERIKOISTUMISEEN_HYVAKSYTTAVA_TUNTIMAARA
-        //     )
-        //     // Lisätään pakollinen tieto
-        //     val erikoistuvaLaakari: ErikoistuvaLaakari
-        //     if (em.findAll(ErikoistuvaLaakari::class).isEmpty()) {
-        //         erikoistuvaLaakari = ErikoistuvaLaakariHelper.createUpdatedEntity(em)
-        //         em.persist(erikoistuvaLaakari)
-        //         em.flush()
-        //     } else {
-        //         erikoistuvaLaakari = em.findAll(ErikoistuvaLaakari::class).get(0)
-        //     }
-        //     teoriakoulutus.opintooikeus = erikoistuvaLaakari.getOpintooikeusKaytossa()
-        //     return teoriakoulutus
-        // }
     }
 }
