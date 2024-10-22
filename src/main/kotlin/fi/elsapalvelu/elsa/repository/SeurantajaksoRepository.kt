44591d49b1890e0bb9f5716a7f7b3386f777f0b2
Satu Kautto <satu.kautto@gofore.com>
2024-10-22 16:05:55 +0300

    Korjaus seurantajaksorepositoryyn

--- src/main/kotlin/fi/elsapalvelu/elsa/repository/SeurantajaksoRepository.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/repository/SeurantajaksoRepository.kt
@@ -1,7 +1,6 @@
 package fi.elsapalvelu.elsa.repository
 
 import fi.elsapalvelu.elsa.domain.Seurantajakso
-import fi.elsapalvelu.elsa.domain.Suoritusarviointi
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
