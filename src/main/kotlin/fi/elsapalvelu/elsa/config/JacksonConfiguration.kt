5f0b8d1a1006d4476493f51658e89517243f5a9d
Arttu Hänninen <arttu.hanninen@gofore.com>
2025-02-10 15:21:45 +0200

    PageImpl -> PagedModel konversio

--- src/main/kotlin/fi/elsapalvelu/elsa/config/JacksonConfiguration.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/config/JacksonConfiguration.kt
@@ -5,8 +5,12 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.web.config.EnableSpringDataWebSupport
 
 @Configuration
+@EnableSpringDataWebSupport(
+    pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
 class JacksonConfiguration {
 
     /**
