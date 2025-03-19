d1380c8cda5673f918ecd218b901aa8c6d5ec87c
Juho Turkki <juho.turkki@gofore.com>
2025-03-19 16:36:15 +0200

    ELSA-1604: Palautetaan kaikki arviointityökalukategoriat kouluttajalle

--- src/main/kotlin/fi/elsapalvelu/elsa/web/rest/kouluttaja/KouluttajaArviointityokalutResource.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/web/rest/kouluttaja/KouluttajaArviointityokalutResource.kt
@@ -1,7 +1,9 @@
 package fi.elsapalvelu.elsa.web.rest.kouluttaja
 
+import fi.elsapalvelu.elsa.service.ArviointityokaluKategoriaService
 import fi.elsapalvelu.elsa.service.ArviointityokaluService
 import fi.elsapalvelu.elsa.service.dto.ArviointityokaluDTO
+import fi.elsapalvelu.elsa.service.dto.ArviointityokaluKategoriaDTO
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/kouluttaja")
 class KouluttajaArviointityokalutResource (
     private val arviointityokaluService: ArviointityokaluService,
+    private val arviointityokaluKategoriaService: ArviointityokaluKategoriaService,
 ) {
     @GetMapping("/arviointityokalut")
     fun getArviointityokalut(): ResponseEntity<List<ArviointityokaluDTO>> {
         return ResponseEntity.ok(arviointityokaluService.findAllJulkaistut())
+    }
+
+    @GetMapping("/arviointityokalut/kategoriat")
+    fun getArviointityokaluKategoriat(): ResponseEntity<List<ArviointityokaluKategoriaDTO>> {
+        return ResponseEntity.ok(arviointityokaluKategoriaService.findAll())
     }
 }
