bd3a9e37cde78bd6d1c40eb969a471d6034781d9
Juho Turkki <juho.turkki@gofore.com>
2025-03-21 14:01:58 +0200

    ELSA-1604 esittely (#460)
    * ELSA-1604: Palauta julkaistut arviointityökalut kouluttajalle
    
    * ELSA-1604: Palautetaan kaikki arviointityökalukategoriat kouluttajalle
    
    ---------
    
    Co-authored-by: Martin Kankaanranta <martin.kankaanranta@gofore.com>

--- src/main/kotlin/fi/elsapalvelu/elsa/service/ArviointityokaluService.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/ArviointityokaluService.kt
@@ -20,6 +20,8 @@ interface ArviointityokaluService {
 
     fun findAll(): List<ArviointityokaluDTO>
 
+    fun findAllJulkaistut(): List<ArviointityokaluDTO>
+
     fun findAllJulkaistu(): List<ArviointityokaluDTO>
 
     fun findAllByKayttajaUserId(userId: String): MutableList<ArviointityokaluDTO>
--- src/main/kotlin/fi/elsapalvelu/elsa/service/impl/ArviointityokaluServiceImpl.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/impl/ArviointityokaluServiceImpl.kt
@@ -125,6 +125,11 @@ class ArviointityokaluServiceImpl(
     }
 
     @Transactional(readOnly = true)
+    override fun findAllJulkaistut(): List<ArviointityokaluDTO> {
+        return arviointityokaluRepository.findAllJulkaistut()
+    }
+        
+    @Transactional(readOnly = true)    
     override fun findAllJulkaistu(): List<ArviointityokaluDTO> {
         return arviointityokaluRepository.findAllByKaytossaTrue()
             .filter { it.tila == ArviointityokalunTila.JULKAISTU }
