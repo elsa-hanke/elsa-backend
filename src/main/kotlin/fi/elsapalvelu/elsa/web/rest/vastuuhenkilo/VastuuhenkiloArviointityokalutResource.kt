cffdeeaf051d5cfb0d03f3ecc68de9cf81449fc4
Martin Kankaanranta <martin.kankaanranta@gofore.com>
2025-09-02 14:37:29 +0300

    ELSA-989: Korjaus arviointityokalujen esittely vastuuhenkilöllä

---
+++ src/main/kotlin/fi/elsapalvelu/elsa/web/rest/vastuuhenkilo/VastuuhenkiloArviointityokalutResource.kt
@@ -0,0 +1,27 @@
+package fi.elsapalvelu.elsa.web.rest.vastuuhenkilo
+
+import fi.elsapalvelu.elsa.service.ArviointityokaluKategoriaService
+import fi.elsapalvelu.elsa.service.ArviointityokaluService
+import fi.elsapalvelu.elsa.service.dto.ArviointityokaluDTO
+import fi.elsapalvelu.elsa.service.dto.ArviointityokaluKategoriaDTO
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/vastuuhenkilo")
+class VastuuhenkiloArviointityokalutResource (
+    private val arviointityokaluService: ArviointityokaluService,
+    private val arviointityokaluKategoriaService: ArviointityokaluKategoriaService,
+) {
+    @GetMapping("/arviointityokalut")
+    fun getArviointityokalut(): ResponseEntity<List<ArviointityokaluDTO>> {
+        return ResponseEntity.ok(arviointityokaluService.findAllJulkaistut())
+    }
+
+    @GetMapping("/arviointityokalut/kategoriat")
+    fun getArviointityokaluKategoriat(): ResponseEntity<List<ArviointityokaluKategoriaDTO>> {
+        return ResponseEntity.ok(arviointityokaluKategoriaService.findAll())
+    }
+}
