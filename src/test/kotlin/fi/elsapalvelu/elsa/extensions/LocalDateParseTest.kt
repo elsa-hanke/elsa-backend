c5dddfdc4e1d88f3b1e1e29d1c7e170da35976f3
netum-alexei <149686391+netum-alexei@users.noreply.github.com>
2026-03-05 10:24:45 +0200

    Revert "Lisätty yksikkötestejä laajasti: FileValidationService, LocalDateExte…"
    This reverts commit d4eb33f8c09b56ac60a0c949a308d66b9b4328cc.

--- src/test/kotlin/fi/elsapalvelu/elsa/extensions/LocalDateParseTest.kt
+++ src/test/kotlin/fi/elsapalvelu/elsa/extensions/LocalDateParseTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.boot.test.context.SpringBootTest
 
+@SpringBootTest(classes = [ElsaBackendApp::class])
 class LocalDateParseTest {
 
     @ParameterizedTest
