e654e06d9575556a0d9980a67b4a2c05d7ac05d6
Alexei Budko <alexei.budko@netum.fi>
2026-03-24 16:59:19 +0200

    fix: update application.yml to use environment variables for SAML and SISUHY configurations

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:9060',
+    baseUrl: 'http://localhost:8080',
     specPattern: 'cypress/e2e/**/*.cy.ts',
     supportFile: 'cypress/support/e2e.ts',
     setupNodeEvents(on, config) {
