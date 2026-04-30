ed38b7079bcafa0e3c3eb0a26b95cfb37c3b7140
Alexei Budko <alexei.budko@netum.fi>
2026-04-30 19:03:43 +0300

    feat: add external integration test suite for Peppi Turku and CI support

--- src/test/kotlin/fi/elsapalvelu/elsa/service/PeppiTurkuExternalIntegrationIT.kt
+++ src/test/kotlin/fi/elsapalvelu/elsa/service/PeppiTurkuExternalIntegrationIT.kt
@@ -1,7 +1,7 @@
 package fi.elsapalvelu.elsa.service
 
-import fi.elsapalvelu.elsa.ElsaBackendApp
 import fi.elsapalvelu.elsa.config.ApplicationProperties
+import fi.elsapalvelu.elsa.service.impl.PeppiTurkuClientBuilderImpl
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.assertj.core.api.Assertions.assertThat
@@ -10,11 +10,14 @@ import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 
 @Tag("external-integration")
-@SpringBootTest(classes = [ElsaBackendApp::class])
+@SpringBootTest(classes = [PeppiTurkuExternalIntegrationIT.ExternalIntegrationTestApplication::class])
 @ActiveProfiles("external-integration")
 class PeppiTurkuExternalIntegrationIT {
 
@@ -54,4 +57,9 @@ class PeppiTurkuExternalIntegrationIT {
     companion object {
         private const val HETU = "030884-227C"
     }
+
+    @SpringBootConfiguration
+    @EnableConfigurationProperties(ApplicationProperties::class)
+    @Import(PeppiTurkuClientBuilderImpl::class)
+    class ExternalIntegrationTestApplication
 }
