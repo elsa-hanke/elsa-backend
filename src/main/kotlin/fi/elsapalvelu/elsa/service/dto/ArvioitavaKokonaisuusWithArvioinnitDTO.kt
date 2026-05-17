3e7ca7333cfd0575f4e1f5cee12de3ebaf2b34b0
Alexei Budko <alexei.budko@netum.fi>
2026-05-17 17:38:27 +0300

    .

--- src/main/kotlin/fi/elsapalvelu/elsa/service/dto/ArvioitavaKokonaisuusWithArvioinnitDTO.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/dto/ArvioitavaKokonaisuusWithArvioinnitDTO.kt
@@ -3,27 +3,38 @@ package fi.elsapalvelu.elsa.service.dto
 import java.io.Serializable
 import java.time.LocalDate
 import jakarta.validation.constraints.NotNull
-import fi.elsapalvelu.elsa.service.dto.SuoritusarviointiByKokonaisuusDTO
 
-data class ArvioitavaKokonaisuusWithArvioinnitDTO : Serializable {
-    var id: Long? = null
-    @get: NotNull
-    var nimi: String? = null
-    var nimiSv: String? = null
-    var kuvaus: String? = null
-    var kuvausSv: String? = null
-    @get: NotNull
-    var voimassaoloAlkaa: LocalDate? = null
-    var voimassaoloLoppuu: LocalDate? = null
-    var suoritusarvioinnit: List<SuoritusarviointiByKokonaisuusDTO> = listOf()
+data class ArvioitavaKokonaisuusWithArvioinnitDTO(
 
-    companion object {
-        private const val serialVersionUID = 1L
-    }
+    var id: Long? = null,
+
+    @get: NotNull
+    var nimi: String? = null,
+
+    var nimiSv: String? = null,
+
+    var kuvaus: String? = null,
+
+    var kuvausSv: String? = null,
+
+    @get: NotNull
+    var voimassaoloAlkaa: LocalDate? = null,
+
+    var voimassaoloLoppuu: LocalDate? = null,
+
+    var suoritusarvioinnit: List<SuoritusarviointiByKokonaisuusDTO?>? = listOf()
+
+) : Serializable {
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is ArvioitavaKokonaisuusWithArvioinnitDTO) return false
         return id != null && id == other.id
     }
+
     override fun hashCode() = 31
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
 }
