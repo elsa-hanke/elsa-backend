8a4551b36eefc3103fc72974f1207faf65dc7f7c
Alexei Budko <alexei.budko@netum.fi>
2026-05-18 14:20:43 +0300

    .

--- src/test/kotlin/fi/elsapalvelu/elsa/web/rest/helpers/KoulutussuunnitelmaHelper.kt
+++ src/test/kotlin/fi/elsapalvelu/elsa/web/rest/helpers/KoulutussuunnitelmaHelper.kt
@@ -7,108 +7,117 @@ import fi.elsapalvelu.elsa.web.rest.createByteArray
 import fi.elsapalvelu.elsa.web.rest.findAll
 import jakarta.persistence.EntityManager
 
-class KoulutussuunnitelmaHelper {
+object KoulutussuunnitelmaHelper {
 
-    companion object {
+    const val DEFAULT_MOTIVAATIOKIRJE = "AAAAAAAAAA"
+    const val UPDATED_MOTIVAATIOKIRJE = "BBBBBBBBBB"
 
-        const val DEFAULT_MOTIVAATIOKIRJE = "AAAAAAAAAA"
-        const val UPDATED_MOTIVAATIOKIRJE = "BBBBBBBBBB"
+    const val DEFAULT_MOTIVAATIOKIRJE_YKSITYINEN = false
+    const val UPDATED_MOTIVAATIOKIRJE_YKSITYINEN = true
 
-        const val DEFAULT_MOTIVAATIOKIRJE_YKSITYINEN: Boolean = false
-        const val UPDATED_MOTIVAATIOKIRJE_YKSITYINEN: Boolean = true
+    const val DEFAULT_OPISKELU_JA_TYOHISTORIA = "AAAAAAAAAA"
+    const val UPDATED_OPISKELU_JA_TYOHISTORIA = "BBBBBBBBBB"
 
-        const val DEFAULT_OPISKELU_JA_TYOHISTORIA = "AAAAAAAAAA"
-        const val UPDATED_OPISKELU_JA_TYOHISTORIA = "BBBBBBBBBB"
+    const val DEFAULT_OPISKELU_JA_TYOHISTORIA_YKSITYINEN = false
+    const val UPDATED_OPISKELU_JA_TYOHISTORIA_YKSITYINEN = true
 
-        const val DEFAULT_OPISKELU_JA_TYOHISTORIA_YKSITYINEN: Boolean = false
-        const val UPDATED_OPISKELU_JA_TYOHISTORIA_YKSITYINEN: Boolean = true
+    const val DEFAULT_VAHVUUDET = "AAAAAAAAAA"
+    const val UPDATED_VAHVUUDET = "BBBBBBBBBB"
 
-        const val DEFAULT_VAHVUUDET = "AAAAAAAAAA"
-        const val UPDATED_VAHVUUDET = "BBBBBBBBBB"
+    const val DEFAULT_VAHVUUDET_YKSITYINEN = false
+    const val UPDATED_VAHVUUDET_YKSITYINEN = true
 
-        const val DEFAULT_VAHVUUDET_YKSITYINEN: Boolean = false
-        const val UPDATED_VAHVUUDET_YKSITYINEN: Boolean = true
+    const val DEFAULT_TULEVAISUUDEN_VISIOINTI = "AAAAAAAAAA"
+    const val UPDATED_TULEVAISUUDEN_VISIOINTI = "BBBBBBBBBB"
 
-        const val DEFAULT_TULEVAISUUDEN_VISIOINTI = "AAAAAAAAAA"
-        const val UPDATED_TULEVAISUUDEN_VISIOINTI = "BBBBBBBBBB"
+    const val DEFAULT_TULEVAISUUDEN_VISIOINTI_YKSITYINEN = false
+    const val UPDATED_TULEVAISUUDEN_VISIOINTI_YKSITYINEN = true
 
-        const val DEFAULT_TULEVAISUUDEN_VISIOINTI_YKSITYINEN: Boolean = false
-        const val UPDATED_TULEVAISUUDEN_VISIOINTI_YKSITYINEN: Boolean = true
+    const val DEFAULT_OSAAMISEN_KARTUTTAMINEN = "AAAAAAAAAA"
+    const val UPDATED_OSAAMISEN_KARTUTTAMINEN = "BBBBBBBBBB"
 
-        const val DEFAULT_OSAAMISEN_KARTUTTAMINEN = "AAAAAAAAAA"
-        const val UPDATED_OSAAMISEN_KARTUTTAMINEN = "BBBBBBBBBB"
+    const val DEFAULT_OSAAMISEN_KARTUTTAMINEN_YKSITYINEN = false
+    const val UPDATED_OSAAMISEN_KARTUTTAMINEN_YKSITYINEN = true
 
-        const val DEFAULT_OSAAMISEN_KARTUTTAMINEN_YKSITYINEN: Boolean = false
-        const val UPDATED_OSAAMISEN_KARTUTTAMINEN_YKSITYINEN: Boolean = true
+    const val DEFAULT_ELAMANKENTTA = "AAAAAAAAAA"
+    const val UPDATED_ELAMANKENTTA = "BBBBBBBBBB"
 
-        const val DEFAULT_ELAMANKENTTA = "AAAAAAAAAA"
-        const val UPDATED_ELAMANKENTTA = "BBBBBBBBBB"
+    const val DEFAULT_ELAMANKENTTA_YKSITYINEN = false
+    const val UPDATED_ELAMANKENTTA_YKSITYINEN = true
 
-        const val DEFAULT_ELAMANKENTTA_YKSITYINEN: Boolean = false
-        const val UPDATED_ELAMANKENTTA_YKSITYINEN: Boolean = true
+    val DEFAULT_FILE: ByteArray = createByteArray(1, "0")
+    val UPDATED_FILE: ByteArray = createByteArray(1, "1")
 
-        val DEFAULT_FILE: ByteArray = createByteArray(1, "0")
-        val UPDATED_FILE: ByteArray = createByteArray(1, "1")
+    fun createEntity(em: EntityManager, user: User? = null): Koulutussuunnitelma {
 
-        @JvmStatic
-        fun createEntity(em: EntityManager, user: User? = null): Koulutussuunnitelma {
-            val koulutussuunnitelma = Koulutussuunnitelma(
-                motivaatiokirje = DEFAULT_MOTIVAATIOKIRJE,
-                motivaatiokirjeYksityinen = DEFAULT_MOTIVAATIOKIRJE_YKSITYINEN,
-                opiskeluJaTyohistoria = DEFAULT_OPISKELU_JA_TYOHISTORIA,
-                opiskeluJaTyohistoriaYksityinen = DEFAULT_OPISKELU_JA_TYOHISTORIA_YKSITYINEN,
-                vahvuudet = DEFAULT_VAHVUUDET,
-                vahvuudetYksityinen = DEFAULT_VAHVUUDET_YKSITYINEN,
-                tulevaisuudenVisiointi = DEFAULT_TULEVAISUUDEN_VISIOINTI,
-                tulevaisuudenVisiointiYksityinen = DEFAULT_TULEVAISUUDEN_VISIOINTI_YKSITYINEN,
-                osaamisenKartuttaminen = DEFAULT_OSAAMISEN_KARTUTTAMINEN,
-                osaamisenKartuttaminenYksityinen = DEFAULT_OSAAMISEN_KARTUTTAMINEN_YKSITYINEN,
-                elamankentta = DEFAULT_ELAMANKENTTA,
-                elamankenttaYksityinen = DEFAULT_ELAMANKENTTA_YKSITYINEN
-            )
+        val koulutussuunnitelma = Koulutussuunnitelma(
+            motivaatiokirje = DEFAULT_MOTIVAATIOKIRJE,
+            motivaatiokirjeYksityinen = DEFAULT_MOTIVAATIOKIRJE_YKSITYINEN,
+            opiskeluJaTyohistoria = DEFAULT_OPISKELU_JA_TYOHISTORIA,
+            opiskeluJaTyohistoriaYksityinen = DEFAULT_OPISKELU_JA_TYOHISTORIA_YKSITYINEN,
+            vahvuudet = DEFAULT_VAHVUUDET,
+            vahvuudetYksityinen = DEFAULT_VAHVUUDET_YKSITYINEN,
+            tulevaisuudenVisiointi = DEFAULT_TULEVAISUUDEN_VISIOINTI,
+            tulevaisuudenVisiointiYksityinen = DEFAULT_TULEVAISUUDEN_VISIOINTI_YKSITYINEN,
+            osaamisenKartuttaminen = DEFAULT_OSAAMISEN_KARTUTTAMINEN,
+            osaamisenKartuttaminenYksityinen = DEFAULT_OSAAMISEN_KARTUTTAMINEN_YKSITYINEN,
+            elamankentta = DEFAULT_ELAMANKENTTA,
+            elamankenttaYksityinen = DEFAULT_ELAMANKENTTA_YKSITYINEN
+        )
 
-            // Lisätään pakollinen tieto
-            var erikoistuvaLaakari =
-                em.findAll(ErikoistuvaLaakari::class).firstOrNull { it.kayttaja?.user == user }
-            if (erikoistuvaLaakari == null) {
-                erikoistuvaLaakari = ErikoistuvaLaakariHelper.createEntity(em, user)
-                em.persist(erikoistuvaLaakari)
-                em.flush()
-            }
-            koulutussuunnitelma.opintooikeus = erikoistuvaLaakari.getOpintooikeusKaytossa()
+        koulutussuunnitelma.opintooikeus =
+            resolveErikoistuvaLaakari(em, user, updated = false)
+                .getOpintooikeusKaytossa()
 
-            return koulutussuunnitelma
-        }
+        return koulutussuunnitelma
+    }
 
-        @JvmStatic
-        fun createUpdatedEntity(em: EntityManager): Koulutussuunnitelma {
-            val koulutussuunnitelma = Koulutussuunnitelma(
-                motivaatiokirje = UPDATED_MOTIVAATIOKIRJE,
-                motivaatiokirjeYksityinen = UPDATED_MOTIVAATIOKIRJE_YKSITYINEN,
-                opiskeluJaTyohistoria = UPDATED_OPISKELU_JA_TYOHISTORIA,
-                opiskeluJaTyohistoriaYksityinen = UPDATED_OPISKELU_JA_TYOHISTORIA_YKSITYINEN,
-                vahvuudet = UPDATED_VAHVUUDET,
-                vahvuudetYksityinen = UPDATED_VAHVUUDET_YKSITYINEN,
-                tulevaisuudenVisiointi = UPDATED_TULEVAISUUDEN_VISIOINTI,
-                tulevaisuudenVisiointiYksityinen = UPDATED_TULEVAISUUDEN_VISIOINTI_YKSITYINEN,
-                osaamisenKartuttaminen = UPDATED_OSAAMISEN_KARTUTTAMINEN,
-                osaamisenKartuttaminenYksityinen = UPDATED_OSAAMISEN_KARTUTTAMINEN_YKSITYINEN,
-                elamankentta = UPDATED_ELAMANKENTTA,
-                elamankenttaYksityinen = UPDATED_ELAMANKENTTA_YKSITYINEN
-            )
+    fun createUpdatedEntity(em: EntityManager): Koulutussuunnitelma {
 
-            // Lisätään pakollinen tieto
-            val erikoistuvaLaakari: ErikoistuvaLaakari
-            if (em.findAll(ErikoistuvaLaakari::class).isEmpty()) {
-                erikoistuvaLaakari = ErikoistuvaLaakariHelper.createUpdatedEntity(em)
-                em.persist(erikoistuvaLaakari)
-                em.flush()
+        val koulutussuunnitelma = Koulutussuunnitelma(
+            motivaatiokirje = UPDATED_MOTIVAATIOKIRJE,
+            motivaatiokirjeYksityinen = UPDATED_MOTIVAATIOKIRJE_YKSITYINEN,
+            opiskeluJaTyohistoria = UPDATED_OPISKELU_JA_TYOHISTORIA,
+            opiskeluJaTyohistoriaYksityinen = UPDATED_OPISKELU_JA_TYOHISTORIA_YKSITYINEN,
+            vahvuudet = UPDATED_VAHVUUDET,
+            vahvuudetYksityinen = UPDATED_VAHVUUDET_YKSITYINEN,
+            tulevaisuudenVisiointi = UPDATED_TULEVAISUUDEN_VISIOINTI,
+            tulevaisuudenVisiointiYksityinen = UPDATED_TULEVAISUUDEN_VISIOINTI_YKSITYINEN,
+            osaamisenKartuttaminen = UPDATED_OSAAMISEN_KARTUTTAMINEN,
+            osaamisenKartuttaminenYksityinen = UPDATED_OSAAMISEN_KARTUTTAMINEN_YKSITYINEN,
+            elamankentta = UPDATED_ELAMANKENTTA,
+            elamankenttaYksityinen = UPDATED_ELAMANKENTTA_YKSITYINEN
+        )
+
+        koulutussuunnitelma.opintooikeus =
+            resolveErikoistuvaLaakari(em, updated = true)
+                .getOpintooikeusKaytossa()
+
+        return koulutussuunnitelma
+    }
+
+    private fun resolveErikoistuvaLaakari(
+        em: EntityManager,
+        user: User? = null,
+        updated: Boolean
+    ): ErikoistuvaLaakari {
+
+        val existing =
+            em.findAll(ErikoistuvaLaakari::class)
+                .firstOrNull { it.kayttaja?.user == user }
+
+        if (existing != null) return existing
+
+        val created =
+            if (updated) {
+                ErikoistuvaLaakariHelper.createUpdatedEntity(em)
             } else {
-                erikoistuvaLaakari = em.findAll(ErikoistuvaLaakari::class).get(0)
+                ErikoistuvaLaakariHelper.createEntity(em, user)
             }
-            koulutussuunnitelma.opintooikeus = erikoistuvaLaakari.getOpintooikeusKaytossa()
 
-            return koulutussuunnitelma
-        }
+        em.persist(created)
+        em.flush()
+
+        return created
     }
 }
