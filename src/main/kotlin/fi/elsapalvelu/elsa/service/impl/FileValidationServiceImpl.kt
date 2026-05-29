fb67949d1c67e67639536211d7c727b7dd7d9fbb
Jouko Johansson <76950886+netum-jouko@users.noreply.github.com>
2026-05-29 14:54:05 +0300

    ELSA-1145: Lisätyt logitusta tiedoston validointiin, jotta pystytään tunnistamaan miksi tiedostoa ei hyväksytä.

--- src/main/kotlin/fi/elsapalvelu/elsa/service/impl/FileValidationServiceImpl.kt
+++ src/main/kotlin/fi/elsapalvelu/elsa/service/impl/FileValidationServiceImpl.kt
@@ -1,7 +1,9 @@
 package fi.elsapalvelu.elsa.service.impl
 
+import fi.elsapalvelu.elsa.security.SecurityLoggingWrapper
 import fi.elsapalvelu.elsa.service.AsiakirjaService
 import fi.elsapalvelu.elsa.service.FileValidationService
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
@@ -13,6 +15,8 @@ class FileValidationServiceImpl(
     private val asiakirjaService: AsiakirjaService
 ) : FileValidationService {
 
+    private val log = LoggerFactory.getLogger(FileValidationServiceImpl::class.java)
+
     private val defaultAllowedContentTypes: List<String> =
         listOf(MediaType.APPLICATION_PDF_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE, "image/jpg")
 
@@ -23,13 +27,23 @@ class FileValidationServiceImpl(
     ): Boolean {
         val allowedContentTypesOrDefault = allowedContentTypes ?: defaultAllowedContentTypes
         val existingFileNames = asiakirjaService.findAllByOpintooikeusId(opintooikeusId).map { it.nimi }
-        if (files.any {
-                it.isEmpty ||
-                    it.originalFilename in existingFileNames ||
-                    it.contentType?.toString() !in allowedContentTypesOrDefault ||
-                    it.name.length > MAXIMUM_FILE_NAME_LENGTH
-            }) {
-            return false
+        files.forEach { file ->
+            if (file.originalFilename.isNullOrBlank()) {
+                log.warn("Tiedoston nimi on tyhjä.")
+                return false
+            }
+            if (file.originalFilename!!.length > MAXIMUM_FILE_NAME_LENGTH) {
+                log.warn("Opintooikeus: ${opintooikeusId} - Tiedoston nimi '${file.originalFilename}' on liian pitkä.")
+                return false
+            }
+            if (!allowedContentTypesOrDefault.contains(file.contentType)) {
+                log.warn("Opintooikeus: ${opintooikeusId} - Tiedoston '${file.originalFilename}' tyyppi '${file.contentType}' ei ole sallittu.")
+                return false
+            }
+            if (existingFileNames.contains(file.originalFilename)) {
+                log.warn("Tiedosto nimeltä '${file.originalFilename}' on jo olemassa opintooikeudella $opintooikeusId.")
+                return false
+            }
         }
 
         return true
