d8500533c4ddf49b63070f8faced733353e242d6
Alexei Budko <alexei.budko@netum.fi>
2026-06-12 17:56:13 +0300

    vue3

--- frontend/src/utils/validators.ts
+++ frontend/src/utils/validators.ts
@@ -91,4 +91,3 @@ export const helpers = {
     }
   }
 }
-
--- frontend/tests/__mocks__/vue-property-decorator.js
+++ frontend/tests/__mocks__/vue-property-decorator.js
@@ -5,12 +5,11 @@
  * as the plain Vue 3 namespace object (not constructors/functions) from its UMD
  * build. This shim overrides them with the proper exports from vue-class-component.
  */
-const umd = require('vue-property-decorator/lib/index.umd.js')
 const vcc = require('vue-class-component')
+const umd = require('vue-property-decorator/lib/index.umd.js')
 
 module.exports = {
   ...umd,
   Vue: vcc.Vue,
   Component: vcc.Options
 }
-
