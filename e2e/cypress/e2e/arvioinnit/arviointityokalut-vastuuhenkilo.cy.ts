795ffe4f12a54c787671f0e79271c0b7fcb50c3b
Alexei Budko <alexei.budko@netum.fi>
2026-08-05 09:28:30 +0300

    .

---
+++ e2e/cypress/e2e/arvioinnit/arviointityokalut-vastuuhenkilo.cy.ts
@@ -0,0 +1,52 @@
+import { VASTUUHENKILO_EMAIL } from '../../support/commands/credentials'
+
+export {}
+
+// Käyttötapaus: Vastuuhenkilö selaa arviointityökalut-esittelysivua
+// Käyttäjä: Vastuuhenkilö
+// Tavoite: Varmistaa, että vastuuhenkilön arviointityökalut-endpointit toimivat
+//          ArviointityokalutResource-kantaluokan refaktoroinnin jälkeen.
+// Sivu: /arviointityokalut/esittely  (allowedRoles: Kouluttaja, Vastuuhenkilo)
+// Testatut endpointit (sivun mount() käynnistää):
+//   GET /api/vastuuhenkilo/arviointityokalut
+//   GET /api/vastuuhenkilo/arviointityokalut/kategoriat
+
+const VASTUUHENKILO_ETUNIMI  = 'E2E'
+const VASTUUHENKILO_SUKUNIMI = 'ArviointityokaluVastuuhenkilo'
+
+describe('Arviointityökalut – vastuuhenkilö', () => {
+  before(() => {
+    Cypress.session.clearAllSavedSessions()
+
+    cy.task('db:seedVastuuhenkilo', {
+      email: VASTUUHENKILO_EMAIL,
+      etunimi: VASTUUHENKILO_ETUNIMI,
+      sukunimi: VASTUUHENKILO_SUKUNIMI,
+    }).then((result: any) => {
+      Cypress.env('vastuuhenkiloToken', result?.token)
+    })
+  })
+
+  it('Vastuuhenkilö avaa arviointityökalut-esittelysivun ja sivu lataa API-datat onnistuneesti', () => {
+    cy.loginAsVastuuhenkilo(Cypress.env('vastuuhenkiloToken'))
+
+    cy.intercept('GET', '**/vastuuhenkilo/arviointityokalut/kategoriat').as('getKategoriat')
+    cy.intercept('GET', '**/vastuuhenkilo/arviointityokalut').as('getArviointityokalut')
+
+    cy.visit('/arviointityokalut/esittely')
+
+    cy.wait('@getKategoriat').then(({ response }) => {
+      expect(response?.statusCode).to.eq(200)
+      expect(response?.body).to.be.an('array')
+    })
+
+    cy.wait('@getArviointityokalut').then(({ response }) => {
+      expect(response?.statusCode).to.eq(200)
+      expect(response?.body).to.be.an('array')
+    })
+
+    // Varmistetaan, että sivu on renderöitynyt oikein
+    cy.contains('h1', 'Arviointityökalut').should('be.visible')
+    cy.get('[role="status"]').should('not.exist')
+  })
+})
