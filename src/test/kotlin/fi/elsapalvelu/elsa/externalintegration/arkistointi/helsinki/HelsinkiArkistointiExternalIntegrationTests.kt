80e9cdfbaf76a287b5e9374c5e75fcba18b6191a
Alexei Budko <alexei.budko@netum.fi>
2026-08-13 14:42:30 +0300

    feat: add alert publishing for authentication failures in SisuTreAuthenticationTokenServiceImpl

--- src/test/kotlin/fi/elsapalvelu/elsa/externalintegration/arkistointi/helsinki/HelsinkiArkistointiExternalIntegrationTests.kt
+++ src/test/kotlin/fi/elsapalvelu/elsa/externalintegration/arkistointi/helsinki/HelsinkiArkistointiExternalIntegrationTests.kt
@@ -19,7 +19,6 @@ import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatCode
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.Mockito
